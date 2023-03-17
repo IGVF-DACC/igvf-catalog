4e50e8062714ded6a5d9a05cf85b6cf9ea30f375
Mingjie Li <mingjiecn@gmail.com>
2023-03-17 11:16:35 -0500

    load protein node and translates_to edge

---
+++ data/adapters/uniprot_adapter.py
@@ -0,0 +1,59 @@
+import gzip
+from Bio import SeqIO
+from adapters import Adapter
+
+# Data file is uniprot_sprot_human.dat.gz at https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/taxonomic_divisions/. 
+# We can use SeqIO from Bio to read the file.
+# Each record in file will have those attributes: https://biopython.org/docs/1.75/api/Bio.SeqRecord.html
+# id, name will be loaded for protein. Ensembl IDs(example: Ensembl:ENST00000372839.7) in dbxrefs will be used to create protein and transcript relationship. 
+
+
+class Uniprot(Adapter):
+  
+  ALLOWED_TYPES = ['protein', 'translates_to']
+
+  def __init__(self, filepath, type='protein'):
+    if type not in Uniprot.ALLOWED_TYPES:
+      raise ValueError('Ivalid type. Allowed values: ' + ', '.join(Uniprot.ALLOWED_TYPES))
+    self.filepath = filepath
+    self.dataset = type
+    self.type = type
+    
+    super(Uniprot, self).__init__()
+
+
+  def process_file(self):
+    with gzip.open(self.filepath, 'rt') as input_file:     
+      records = SeqIO.parse(input_file, "swiss")
+      label = self.type
+      if self.type == 'protein':
+        for record in records:  
+            try:
+                _id = record.id
+                _props = {
+                'name': record.name,
+                }
+                yield(_id, label, _props)
+
+            except:
+                print(f'fail to process for node protein: {record.id}')
+                pass
+      elif self.typ == 'translates_to':
+        edges = []
+        dbxrefs = record.dbxrefs
+        try:
+          for item in dbxrefs:
+            if item.startswith("Ensembl") and "ENSG" in item:
+              ensg_id = item.split(":")[-1]
+              _id = record.id + '_' + ensg_id
+              _source = 'variants/' + ensg_id
+              _target = 'proteins/' + record.id
+              edges.append((_id, _source, _target, label))
+          if edges:
+            yield from edges
+        except:
+          print(f'fail to process for edge translates to: {record.id}')
+          pass
+
+
+
