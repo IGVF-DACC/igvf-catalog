8021b8dbeccc6853cef6230419b61ad4d9d62c29
Mingjie Li <mingjiecn@gmail.com>
2023-03-28 15:36:25 -0500

    pre-commit

--- data/dev_setup.py
+++ data/dev_setup.py
@@ -14,40 +14,44 @@ from db.arango_db import ArangoDB
 
 
 ADAPTERS = {
-  'gencode_genes': Gencode(filepath='./samples/gencode_sample.gtf', type='gene', chr='chr1'),
-  'gencode_transcripts': Gencode(filepath='./samples/gencode_sample.gtf', type='transcript', chr='chr1'),
-  'gnomad': Gnomad(filepath='./samples/gnomad_sample.vcf', chr='chrY'),
-  'topld': TopLD(filepath='./samples/topld_sample.csv', chr='chr22', ancestry='SAS'),
-  'eqtl': EQtl(filepath='./samples/qtl_sample.txt', biological_context='brain_amigdala'),
-  'caqtl': CAQtl(filepath='./samples/caqtl-sample.bed'),
-  'caqtl_ocr': CAQtl(filepath='./samples/caqtl-sample.bed', type='accessible_dna_region'),
-  'ccre': CCRE(filepath='./samples/ccre_example.bed.gz')
-  'clo_nodes': Ontology(ontology='CLO', type='node'),
-  'clo_edges': Ontology(ontology='CLO', type='edge'),
-  'uberon_nodes': Ontology(ontology='UBERON', type='node'),
-  'uberon_edges': Ontology(ontology='UBERON', type='edge'),
-  'cl_nodes': Ontology(ontology='CL', type='node'),
-  'cl_edges': Ontology(ontology='CL', type='edge'),
-  'hpo_nodes': Ontology(ontology='HPO', type='node'),
-  'hpo_edges': Ontology(ontology='HPO', type='edge'),
-  'mondo_nodes': Ontology(ontology='MONDO', type='node'),
-  'mondo_edges': Ontology(ontology='MONDO', type='edge'),
-  'go_nodes': Ontology(ontology='GO', type='node'),
-  'go_edges': Ontology(ontology='GO', type='edge')
-  'ccre': CCRE(filepath='./samples/ccre_example.bed.gz'),
-  'uniprot': Uniprot(filepath='./samples/uniprot_sprot_human_sample.dat.gz'),
-  'translates_to': Uniprot(filepath='./samples/uniprot_sprot_human_sample.dat.gz', type='translates_to')
+    'gencode_genes': Gencode(filepath='./samples/gencode_sample.gtf', type='gene', chr='chr1'),
+    'gencode_transcripts': Gencode(filepath='./samples/gencode_sample.gtf', type='transcript', chr='chr1'),
+    'gnomad': Gnomad(filepath='./samples/gnomad_sample.vcf', chr='chrY'),
+    'topld': TopLD(filepath='./samples/topld_sample.csv', chr='chr22', ancestry='SAS'),
+    'eqtl': EQtl(filepath='./samples/qtl_sample.txt', biological_context='brain_amigdala'),
+    'caqtl': CAQtl(filepath='./samples/caqtl-sample.bed'),
+    'caqtl_ocr': CAQtl(filepath='./samples/caqtl-sample.bed', type='accessible_dna_region'),
+    'ccre': CCRE(filepath='./samples/ccre_example.bed.gz')
+    'clo_nodes': Ontology(ontology='CLO', type='node'),
+    'clo_edges': Ontology(ontology='CLO', type='edge'),
+    'uberon_nodes': Ontology(ontology='UBERON', type='node'),
+    'uberon_edges': Ontology(ontology='UBERON', type='edge'),
+    'cl_nodes': Ontology(ontology='CL', type='node'),
+    'cl_edges': Ontology(ontology='CL', type='edge'),
+    'hpo_nodes': Ontology(ontology='HPO', type='node'),
+    'hpo_edges': Ontology(ontology='HPO', type='edge'),
+    'mondo_nodes': Ontology(ontology='MONDO', type='node'),
+    'mondo_edges': Ontology(ontology='MONDO', type='edge'),
+    'go_nodes': Ontology(ontology='GO', type='node'),
+    'go_edges': Ontology(ontology='GO', type='edge')
+    'ccre': CCRE(filepath='./samples/ccre_example.bed.gz'),
+    'uniprot': Uniprot(filepath='./samples/uniprot_sprot_human_sample.dat.gz'),
+    'translates_to': Uniprot(filepath='./samples/uniprot_sprot_human_sample.dat.gz', type='translates_to')
 }
 
 parser = argparse.ArgumentParser(
     prog='IGVF Catalog Sample Data Loader',
     description='Loads sample data into a local ArangoDB instance'
-  )
+)
 
-parser.add_argument('--dry-run', action='store_true', help = 'Dry Run / Print ArangoDB Statements')
-parser.add_argument("-a", "--adapter", nargs='*', help = "Loads the sampe adata for an adapter", choices=ADAPTERS.keys())
-parser.add_argument("-i", "--create-indexes", action='store_true', help = "Creates ArangoDB indexes for a given adapter")
-parser.add_argument("-l", "--create-aliases", action='store_true', help = "Creates ArangoDB fuzzy search alisases for a given adapter")
+parser.add_argument('--dry-run', action='store_true',
+                    help='Dry Run / Print ArangoDB Statements')
+parser.add_argument('-a', '--adapter', nargs='*',
+                    help='Loads the sampe adata for an adapter', choices=ADAPTERS.keys())
+parser.add_argument('-i', '--create-indexes', action='store_true',
+                    help='Creates ArangoDB indexes for a given adapter')
+parser.add_argument('-l', '--create-aliases', action='store_true',
+                    help='Creates ArangoDB fuzzy search alisases for a given adapter')
 
 args = parser.parse_args()
 
@@ -57,28 +61,29 @@ create_aliases = args.create_aliases
 adapters = args.adapter or ADAPTERS.keys()
 
 if not dry_run:
-  ArangoDB().setup_dev()
+    ArangoDB().setup_dev()
 
 import_cmds = []
-  
+
 for a in adapters:
-  adapter = ADAPTERS[a]
+    adapter = ADAPTERS[a]
 
-  if create_indexes:
-    adapter.create_indexes()
-  elif create_aliases:
-    adapter.create_aliases()
-  else:
-    adapter.write_file()
-    import_cmds.append(adapter.arangodb())
+    if create_indexes:
+        adapter.create_indexes()
+    elif create_aliases:
+        adapter.create_aliases()
+    else:
+        adapter.write_file()
+        import_cmds.append(adapter.arangodb())
 
-    if adapter.has_indexes():
-      print('{} needs indexes. After data loading, please run: python3 dev_setup.py -i -a {}'.format(a, a))
+        if adapter.has_indexes():
+            print(
+                '{} needs indexes. After data loading, please run: python3 dev_setup.py -i -a {}'.format(a, a))
 
-import_cmds = sum(import_cmds, []) # [[cmd1], [cmd2]] => [cmd1, cmd2]
+import_cmds = sum(import_cmds, [])  # [[cmd1], [cmd2]] => [cmd1, cmd2]
 
 for cmd in import_cmds:
-  if dry_run:
-    print(cmd)
-  else:
-    os.system(cmd)
+    if dry_run:
+        print(cmd)
+    else:
+        os.system(cmd)
--- data/tests/test_helpers.py
+++ data/tests/test_helpers.py
@@ -1,46 +1,48 @@
 import pytest
 from adapters.helpers import build_variant_id, build_accessible_dna_region_id
 
-def test_build_variant_id_fails_for_unsupported_assembly():
-  with pytest.raises(ValueError, match='Assembly not supported'):
-    build_variant_id(None, None, None, None, 'hg19')
 
-  try:
-    build_variant_id(None, None, None, None, 'GRCh38')
-    build_variant_id(None, None, None, None)
-  except:
-    assert False, 'build_variant_id raised exception for GRCh38'
+def test_build_variant_id_fails_for_unsupported_assembly():
+    with pytest.raises(ValueError, match='Assembly not supported'):
+        build_variant_id(None, None, None, None, 'hg19')
+
+    try:
+        build_variant_id(None, None, None, None, 'GRCh38')
+        build_variant_id(None, None, None, None)
+    except:
+        assert False, 'build_variant_id raised exception for GRCh38'
 
 
 def test_build_variant_id_creates_id_string():
-  chr = 'chr'
-  pos = 'pos'
-  ref_seq = 'ref_seq'
-  alt_seq = 'alt_seq'
-  assembly = 'GRCh38'
+    chr = 'chr'
+    pos = 'pos'
+    ref_seq = 'ref_seq'
+    alt_seq = 'alt_seq'
+    assembly = 'GRCh38'
 
-  variant_id = build_variant_id(chr, pos, ref_seq, alt_seq, assembly)
+    variant_id = build_variant_id(chr, pos, ref_seq, alt_seq, assembly)
 
-  assert variant_id == '{}_{}_{}_{}_{}'.format(chr, pos, ref_seq, alt_seq, 'GRCh38')
+    assert variant_id == '{}_{}_{}_{}_{}'.format(
+        chr, pos, ref_seq, alt_seq, 'GRCh38')
 
 
 def test_build_open_chromatic_region_id_fails_for_unsupported_assembly():
-  with pytest.raises(ValueError, match='Assembly not supported'):
-    build_accessible_dna_region_id(None, None, None, 'hg19')
+    with pytest.raises(ValueError, match='Assembly not supported'):
+        build_accessible_dna_region_id(None, None, None, 'hg19')
 
-  try:
-    build_accessible_dna_region_id(None, None, None, 'GRCh38')
-    build_accessible_dna_region_id(None, None, None)
-  except:
-    assert False, 'build_chromatic_region_id raised exception for GRCh38'
+    try:
+        build_accessible_dna_region_id(None, None, None, 'GRCh38')
+        build_accessible_dna_region_id(None, None, None)
+    except:
+        assert False, 'build_chromatic_region_id raised exception for GRCh38'
 
 
 def test_build_variant_id_creates_id_string():
-  chr = 'chr'
-  pos_start = 'start'
-  pos_end = 'end'
-  assembly = 'GRCh38'
+    chr = 'chr'
+    pos_start = 'start'
+    pos_end = 'end'
+    assembly = 'GRCh38'
 
-  id = build_accessible_dna_region_id(chr, pos_start, pos_end, assembly)
+    id = build_accessible_dna_region_id(chr, pos_start, pos_end, assembly)
 
-  assert id == '{}_{}_{}_{}'.format(chr, pos_start, pos_end, assembly)
+    assert id == '{}_{}_{}_{}'.format(chr, pos_start, pos_end, assembly)
