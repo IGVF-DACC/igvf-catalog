9781282785db2b3e2811d99e687b675c1172c8c8
Mingjie Li <mingjiecn@gmail.com>
2023-07-13 08:57:36 -0500

    use Bio.SwissProt instead of Bio.SeqIO

--- data/adapters/uniprot_protein_adapter.py
+++ data/adapters/uniprot_protein_adapter.py
@@ -1,9 +1,9 @@
 import gzip
 import json
 import os
-from Bio import SeqIO
 from adapters import Adapter
 from db.arango_db import ArangoDB
+from Bio import SwissProt
 
 
 # Data file is uniprot_sprot_human.dat.gz and uniprot_trembl_human.dat.gz at https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/taxonomic_divisions/.
@@ -36,15 +36,36 @@ class UniprotProtein(Adapter):
 
         super(UniprotProtein, self).__init__()
 
+    def get_dbxrefs(self, cross_references):
+        dbxrefs = []
+        for cross_reference in cross_references:
+            database_name = cross_reference[0]
+            if database_name == 'EMBL':
+                for item in cross_reference[1:3]:
+                    if item != '-':
+                        id = database_name + ':' + item
+                        dbxrefs.append(id)
+            elif database_name in ['RefSeq', 'Ensembl', 'MANE-Select']:
+                for item in cross_reference[1:]:
+                    if item != '-':
+                        id = database_name + ':' + item.split('. ')[0]
+                        dbxrefs.append(id)
+            else:
+                id = cross_reference[0] + ':' + cross_reference[1]
+                dbxrefs.append(id)
+
+        return sorted(list(set(dbxrefs)), key=str.casefold)
+
     def process_file(self):
         parsed_data_file = open(self.output_filepath, 'w')
         with gzip.open(self.filepath, 'rt') as input_file:
-            records = SeqIO.parse(input_file, 'swiss')
+            records = SwissProt.parse(input_file)
             for record in records:
+                dbxrefs = self.get_dbxrefs(record.cross_references)
                 to_json = {
-                    '_key': record.id,
-                    'name': record.name,
-                    'dbxrefs': record.dbxrefs,
+                    '_key': record.accessions[0],
+                    'name': record.entry_name,
+                    'dbxrefs': dbxrefs,
                     'source': self.source,
                     'source_url': 'https://www.uniprot.org/help/downloads'
                 }
