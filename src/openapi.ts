9adf45ad90f59ca6de5d0f3c7bd0e92b9b1cec9a
Pedro Assis <pedroh2306@gmail.com>
2023-07-26 11:22:50 -1000

    prevent ssl mismatch in production

--- src/openapi.ts
+++ src/openapi.ts
@@ -2,10 +2,16 @@ import { generateOpenApiDocument } from 'trpc-openapi'
 import { appRouter } from './routers/_app'
 import { envData } from './env'
 
+let baseUrl = `${envData.host.protocol}://${envData.host.hostname}:${envData.host.port}/api`
+// prevents producation SSL cert mismatch
+if (envData.host.port === 80) {
+  baseUrl = `${envData.host.protocol}://${envData.host.hostname}/api`
+}
+
 export const openApiDocument = generateOpenApiDocument(appRouter, {
   title: 'IGVF Catalog',
   description: 'IGVF Catalog OpenAPI compliant REST API built using tRPC with Express',
   version: '0.0.1',
-  baseUrl: `${envData.host.protocol}://${envData.host.hostname}:${envData.host.port}/api`,
-  docsUrl: 'https://github.com/IGVF-DACC/igvf-catalog'
+  docsUrl: 'https://github.com/IGVF-DACC/igvf-catalog',
+  baseUrl
 })
