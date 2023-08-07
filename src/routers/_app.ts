ef172f78dc08ad8450b3d35e0e577b666fda281b
Pedro Assis <pedroh2306@gmail.com>
2023-08-07 09:42:32 -1000

    typo

--- src/routers/_app.ts
+++ src/routers/_app.ts
@@ -3,7 +3,7 @@ import { router } from '../trpc'
 import { nodeRouters } from './datatypeRouters/nodes/_all'
 import { edgeRouters } from './datatypeRouters/edges/_all'
 
-// Edge endpoints must preceed node endpoints to avoid naming conflict
+// Edge endpoints must preceed node endpoints to avoid naming conflicts
 export const appRouter = router({ ...edgeRouters, ...nodeRouters })
 
 export type igvfCatalogRouter = typeof appRouter
