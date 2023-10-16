7c5a8270b2ca06404c4ff44ab8dfd11e272aefcd
shengchd <shengchd@umich.edu>
2023-10-16 14:34:31 -0700

    remove wrong file

--- src/routers/datatypeRouters/edges/variants_proteins.ts
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod'
-import { publicProcedure } from '../../../trpc'
-import { loadSchemaConfig } from '../../genericRouters/genericRouters'
-import { RouterEdges } from '../../genericRouters/routerEdges'
-
-const schema = loadSchemaConfig()
