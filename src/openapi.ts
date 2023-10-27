695b8867fbc18d579b3fdcd2e738796b1280e464
Pedro Assis <pedroh2306@gmail.com>
2023-10-27 11:58:16 -1000

    DSERV-306 Ordering endpoints (#128)
    * ordering endpoints
    
    * enabling try it out by default on swagger

--- src/openapi.ts
+++ src/openapi.ts
@@ -8,6 +8,12 @@ if (envData.host.port === 80) {
   baseUrl = `${envData.host.protocol}://${envData.host.hostname}/api`
 }
 
+export const swaggerConfig = {
+  swaggerOptions: {
+    tryItOutEnabled: true
+  }
+}
+
 let openApiConfig = {
   title: 'IGVF Catalog - Development',
   description: 'Development IGVF Catalog OpenAPI compliant REST API built using tRPC with Express',
@@ -26,4 +32,42 @@ if (process.env.ENV === 'production') {
   }
 }
 
+// Order defined by endpoints that have the following prefixes
+const endpointsOrder = [
+  '/variants',
+  '/genes',
+  '/transcripts',
+  '/proteins',
+  '/regulatory_regions',
+  '/diseases',
+  '/phenotypes',
+  '/ontology_terms',
+  '/go',
+  '/motifs',
+  '/studies',
+  '/'
+]
+
 export const openApiDocument = generateOpenApiDocument(appRouter, openApiConfig)
+
+const endpoints = Object.keys(openApiDocument.paths).sort((a, b) => { return a.length - b.length })
+const endpointsCheck = endpoints.map((endpoint) => [endpoint, false])
+
+const orderedList: string[] = []
+
+endpointsOrder.forEach(prefix => {
+  endpointsCheck.forEach(endpointMap => {
+    if (endpointMap[1] === false && (endpointMap[0] as string).startsWith(prefix)) {
+      orderedList.push(endpointMap[0] as string)
+
+      endpointMap[1] = true
+    }
+  })
+})
+
+const newPath: typeof openApiDocument.paths = {}
+orderedList.forEach((endpoint) => {
+  newPath[endpoint] = openApiDocument.paths[endpoint]
+})
+
+openApiDocument.paths = newPath
