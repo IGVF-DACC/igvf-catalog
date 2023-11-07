8bc3fce3e202d9da9e301500616dc1a9208d8d05
Pedro Assis <pedroh2306@gmail.com>
2023-11-07 10:39:10 -1000

    enabling funseq description

--- src/routers/datatypeRouters/edges/variants_phenotypes.ts
+++ src/routers/datatypeRouters/edges/variants_phenotypes.ts
@@ -106,7 +106,7 @@ const variantsFromPhenotypes = publicProcedure
 
 const phenotypesFromVariants = publicProcedure
   .meta({ openapi: { method: 'GET', path: '/variants/phenotypes', description: descriptions.variants_phenotypes } })
-  .input(variantsQueryFormat.omit({ funseq_description: true }).merge(z.object({ pmid: z.string().optional(), p_value: z.string().optional(), verbose: z.enum(['true', 'false']).default('false') })))
+  .input(variantsQueryFormat.merge(z.object({ pmid: z.string().optional(), p_value: z.string().optional(), verbose: z.enum(['true', 'false']).default('false') })))
   .output(z.array(variantPhenotypeFormat))
   .query(async ({ input }) => await variantSearch(input))
 
