5f6517d037da92070ba6dc3c9340ab2686df59fd
Mingjie Li <44071821+mingjiecn@users.noreply.github.com>
2023-11-09 09:58:29 -0600

    DSERV-313-protein-schema (#133)

--- src/routers/datatypeRouters/nodes/proteins.ts
+++ src/routers/datatypeRouters/nodes/proteins.ts
@@ -11,6 +11,7 @@ const schema = loadSchemaConfig()
 
 export const proteinsQueryFormat = z.object({
   name: z.string().optional(),
+  full_name: z.string().optional(),
   dbxrefs: z.string().optional(),
   page: z.number().default(0)
 })
@@ -18,6 +19,7 @@ export const proteinsQueryFormat = z.object({
 export const proteinFormat = z.object({
   _id: z.string(),
   name: z.string(),
+  full_name: z.string().optional(),
   dbxrefs: z.array(z.string()).optional(),
   source: z.string(),
   source_url: z.string()
