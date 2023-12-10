75bcee9c80d92cba273745ca27f7c7676e572ee5
Mingjie Li <mingjiecn@gmail.com>
2023-12-10 15:27:53 -0600

    update adatper for mouse gene and transcript

--- data/adapters/gencode_adapter.py
+++ data/adapters/gencode_adapter.py
@@ -13,25 +13,28 @@ from adapters import Adapter
 
 
 class Gencode(Adapter):
-    ALLOWED_TYPES = ['transcript',
-                     'transcribed to', 'transcribed from']
     ALLOWED_LABELS = ['gencode_transcript',
+                      'mm_gencode_transcript',
                       'transcribed_to', 'transcribed_from']
     ALLOWED_KEYS = ['gene_id', 'gene_type', 'gene_name',
                     'transcript_id', 'transcript_type', 'transcript_name']
 
     INDEX = {'chr': 0, 'type': 2, 'coord_start': 3, 'coord_end': 4, 'info': 8}
 
-    def __init__(self, filepath=None, type='gene', label='gencode_gene', chr='all'):
+    def __init__(self, filepath=None, label='gencode_transcript', chr='all'):
         if label not in Gencode.ALLOWED_LABELS:
             raise ValueError('Ivalid labelS. Allowed values: ' +
                              ','.join(Gencode.ALLOWED_LABELS))
 
         self.filepath = filepath
-        self.type = type
         self.chr = chr
         self.label = label
-        self.dataset = label
+        if self.label in ['gencode_transcript', 'transcribed_to', 'transcribed_from']:
+            self.version = 'v43'
+            self.source_url = 'https://www.gencodegenes.org/human/'
+        else:
+            self.version = 'vM33'
+            self.source_url = 'https://www.gencodegenes.org/mouse/'
 
         super(Gencode, self).__init__()
 
@@ -50,7 +53,6 @@ class Gencode(Adapter):
             data_line = line.strip().split()
             if data_line[Gencode.INDEX['type']] != 'transcript':
                 continue
-
             data = data_line[:Gencode.INDEX['info']]
             info = self.parse_info_metadata(data_line[Gencode.INDEX['info']:])
             transcript_key = info['transcript_id'].split('.')[0]
@@ -60,41 +62,41 @@ class Gencode(Adapter):
             if info['gene_id'].endswith('_PAR_Y'):
                 gene_key = gene_key + '_PAR_Y'
             try:
-                if self.type == 'transcript':
+                if self.label in ['gencode_transcript', 'mm_gencode_transcript']:
                     props = {
                         'transcript_id': info['transcript_id'],
                         'transcript_name': info['transcript_name'],
                         'transcript_type': info['transcript_type'],
                         'chr': data[Gencode.INDEX['chr']],
                         # the gtf file format is [1-based,1-based], needs to convert to BED format [0-based,1-based]
-                        'start:long': str(int(data[Gencode.INDEX['coord_start']]) - 1),
-                        'end:long': data[Gencode.INDEX['coord_end']],
+                        'start': str(int(data[Gencode.INDEX['coord_start']]) - 1),
+                        'end': data[Gencode.INDEX['coord_end']],
                         'gene_name': info['gene_name'],
                         'source': 'GENCODE',
-                        'version': 'v43',
-                        'source_url': 'https://www.gencodegenes.org/human/'
+                        'version': self.version,
+                        'source_url': self.source_url
                     }
                     yield(transcript_key, self.label, props)
-                elif self.type == 'transcribed to':
+                elif self.label == 'transcribed_to':
                     _id = gene_key + '_' + transcript_key
                     _source = 'genes/' + gene_key
                     _target = 'transcripts/' + transcript_key
                     _props = {
                         'source': 'GENCODE',
-                        'version': 'v43',
-                        'source_url': 'https://www.gencodegenes.org/human/'
+                        'version': self.version,
+                        'source_url': self.source_url
                     }
                     yield(_id, _source, _target, self.label, _props)
-                elif self.type == 'transcribed from':
+                elif self.label == 'transcribed_from':
                     _id = transcript_key + '_' + gene_key
                     _source = 'transcripts/' + transcript_key
                     _target = 'genes/' + gene_key
                     _props = {
                         'source': 'GENCODE',
-                        'version': 'v43',
-                        'source_url': 'https://www.gencodegenes.org/human/'
+                        'version': self.version,
+                        'source_url': self.source_url
                     }
                     yield(_id, _source, _target, self.label, _props)
             except:
                 print(
-                    f'fail to process for label to load: {self.label}, type to load: {self.type}, data: {line}')
+                    f'fail to process for label to load: {self.label}, type to load: {self.label}, data: {line}')
--- data/adapters/gencode_gene_adapter.py
+++ data/adapters/gencode_gene_adapter.py
@@ -6,7 +6,7 @@ from db.arango_db import ArangoDB
 
 
 # Example genocde vcf input file:
-# ##description: evidence-based annotation of the human genome (GRCh38), version 42 (Ensembl 108)
+# ##description: evidence-based annotation of the human genome (GRCh38), version 43 (Ensembl 108)
 # ##provider: GENCODE
 # ##contact: gencode-help@ebi.ac.uk
 # ##format: gtf
@@ -19,25 +19,36 @@ from db.arango_db import ArangoDB
 
 class GencodeGene(Adapter):
     ALLOWED_KEYS = ['gene_id', 'gene_type', 'gene_name',
-                    'transcript_id', 'transcript_type', 'transcript_name', 'hgnc_id']
+                    'transcript_id', 'transcript_type', 'transcript_name', 'hgnc_id', 'mgi_id']
     INDEX = {'chr': 0, 'type': 2, 'coord_start': 3, 'coord_end': 4, 'info': 8}
     OUTPUT_FOLDER = './parsed-data'
+    ALLOWED_LABELS = [
+        'gencode_gene',
+        'mm_gencode_gene',
+    ]
 
-    def __init__(self, filepath=None, gene_alias_file_path=None, chr='all', dry_run=False):
-
+    def __init__(self, filepath=None, gene_alias_file_path=None, chr='all', label='gencode_gene', dry_run=False):
+        if label not in GencodeGene.ALLOWED_LABELS:
+            raise ValueError('Ivalid label. Allowed values: ' +
+                             ','.join(GencodeGene.ALLOWED_LABELS))
         self.filepath = filepath
         self.chr = chr
-        self.label = 'gencode_gene'
-        self.dataset = 'gencode_gene'
+        self.label = label
         self.gene_alias_file_path = gene_alias_file_path
         if not os.path.exists(GencodeGene.OUTPUT_FOLDER):
             os.makedirs(GencodeGene.OUTPUT_FOLDER)
         self.output_filepath = '{}/{}.json'.format(
             GencodeGene.OUTPUT_FOLDER,
-            self.dataset,
+            self.label,
         )
         self.dry_run = dry_run
         self.SKIP_BIOCYPHER = True
+        if self.label == 'gencode_gene':
+            self.version = 'v43'
+            self.source_url = 'https://www.gencodegenes.org/human/'
+        else:
+            self.version = 'vM33'
+            self.source_url = 'https://www.gencodegenes.org/mouse/'
 
         super(GencodeGene, self).__init__()
 
@@ -48,30 +59,30 @@ class GencodeGene(Adapter):
                 parsed_info[key] = value.replace('"', '').replace(';', '')
         return parsed_info
 
-    # the gene alias dict will use both ensembl id and hgnc id as key
+    # the gene alias dict will use both ensembl id and hgnc id (or mgi id for mouse) as key
     def get_collection_alias(self):
         alias_dict = {}
         with gzip.open(self.gene_alias_file_path, 'rt') as input:
             next(input)
             for line in input:
-                (tax_id, gene_id, symbol, locus_tag, synonyms, dbxrefs, chromosome, map_location, description, type_of_gene, symbol_from_nomenclature_authority,
-                 full_name_from_nomenclature_authority, Nomenclature_status, Other_designations, Modification_date, Feature_type) = line.split('\t')
-
-                split_dbxrefs = dbxrefs.split('|')
                 hgnc = ''
                 ensembl = ''
+                mgi = ''
+                (tax_id, gene_id, symbol, locus_tag, synonyms, dbxrefs, chromosome, map_location, description, type_of_gene, symbol_from_nomenclature_authority,
+                 full_name_from_nomenclature_authority, Nomenclature_status, Other_designations, Modification_date, Feature_type) = line.split('\t')
+                split_dbxrefs = dbxrefs.split('|')
                 for ref in split_dbxrefs:
                     if ref.startswith('HGNC:'):
                         hgnc = ref[5:]
-                    if ref.startswith('Ensembl:'):
+                    elif ref.startswith('Ensembl:'):
                         ensembl = ref[8:]
-                if ensembl or hgnc:
+                    elif ref.startswith('MGI:'):
+                        mgi = ref[4:]
+                if ensembl or hgnc or mgi:
                     complete_synonyms = []
                     complete_synonyms.append(symbol)
                     for i in synonyms.split('|'):
                         complete_synonyms.append(i)
-                    if hgnc:
-                        complete_synonyms.append(hgnc)
                     for i in Other_designations.split('|'):
                         complete_synonyms.append(i)
                     complete_synonyms.append(
@@ -81,11 +92,27 @@ class GencodeGene(Adapter):
                     complete_synonyms = list(set(complete_synonyms))
                     if '-' in complete_synonyms:
                         complete_synonyms.remove('-')
+                    alias = {
+                        'alias': complete_synonyms,
+                        'entrez': gene_id
+                    }
+                    if self.label == 'gencode_gene' and hgnc:
+                        alias.update(
+                            {'hgnc': hgnc}
+                        )
+                        alias_dict[hgnc] = alias
+                    if self.label == 'mm_gencode_gene' and mgi:
+                        alias.update(
+                            {'mgi': mgi}
+                        )
+                        alias_dict[mgi] = alias
                     if ensembl:
-                        alias_dict[ensembl] = complete_synonyms
-                    if hgnc:
-                        alias_dict[hgnc] = complete_synonyms
-
+                        alias_dict[ensembl] = alias
+        alias_dict_file = open('{}_alias.json'.format(
+            self.label,
+        ), 'w')
+        json.dump(alias_dict, alias_dict_file)
+        alias_dict_file.write('\n')
         return alias_dict
 
     def get_hgnc_id(self, id, info, alias_dict):
@@ -93,11 +120,27 @@ class GencodeGene(Adapter):
         if not hgnc_id:
             alias = alias_dict.get(id)
             if alias:
-                for item in alias:
-                    if item.startswith('HGNC:'):
-                        hgnc_id = item
-                        break
+                hgnc_id = alias.get('hgnc')
         return hgnc_id
+
+    def get_mgi_id(self, id, info, alias_dict):
+        mgi_id = info.get('mgi_id')
+        if not mgi_id:
+            alias = alias_dict.get(id)
+            if alias:
+                mgi_id = alias.get('mgi')
+        return mgi_id
+
+    def get_alias_by_id(self, id, hgnc_id, mgi_id, alias_dict):
+        for key in [id, hgnc_id, mgi_id]:
+            if key in alias_dict:
+                return alias_dict[key]
+        return None
+
+    def get_entrez_id(self, alias):
+        for item in alias:
+            if item.startswith('ENTREZ:'):
+                return item
 
     def process_file(self):
         alias_dict = self.get_collection_alias()
@@ -112,9 +155,8 @@ class GencodeGene(Adapter):
                 gene_id = info['gene_id']
                 id = gene_id.split('.')[0]
                 hgnc_id = self.get_hgnc_id(id, info, alias_dict)
-                alias = alias_dict.get(id)
-                if not alias and hgnc_id:
-                    alias = alias_dict.get(hgnc_id)
+                mgi_id = self.get_mgi_id(id, info, alias_dict)
+                alias = self.get_alias_by_id(id, hgnc_id, mgi_id, alias_dict)
                 if gene_id.endswith('_PAR_Y'):
                     id = id + '_PAR_Y'
                 to_json = {
@@ -127,21 +169,26 @@ class GencodeGene(Adapter):
                     'end:long': int(split_line[GencodeGene.INDEX['coord_end']]),
                     'gene_name': info['gene_name'],
                     'source': 'GENCODE',
-                    'version': 'v43',
-                    'source_url': 'https://www.gencodegenes.org/human/'
+                    'version': self.version,
+                    'source_url': self.source_url
                 }
                 if hgnc_id:
                     to_json.update(
                         {
-                            'hgnc': hgnc_id
+                            'hgnc': hgnc_id,
                         }
                     )
-                    if alias and hgnc_id in alias:
-                        alias.remove(hgnc_id)
+                if mgi_id:
+                    to_json.update(
+                        {
+                            'mgi': mgi_id,
+                        }
+                    )
                 if alias:
                     to_json.update(
                         {
-                            'alias': alias
+                            'alias': alias['alias'],
+                            'entrez': alias['entrez']
                         }
                     )
                 json.dump(to_json, parsed_data_file)
