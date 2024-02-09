22984f3ea62b03d336e3eafe2fd8fdefa6112f53
shengchd <shengchd@umich.edu>
2024-02-08 16:42:36 -0800

    bug fix

--- src/routers/datatypeRouters/edges/regulatory_regions_biosamples.ts
+++ src/routers/datatypeRouters/edges/regulatory_regions_biosamples.ts
@@ -5,7 +5,6 @@ import { RouterEdges } from '../../genericRouters/routerEdges'
 import { paramsFormatType } from '../_helpers'
 import { ontologyFormat } from '../nodes/ontologies'
 import { regulatoryRegionFormat, regulatoryRegionsQueryFormat } from '../nodes/regulatory_regions'
-import { TRPCError } from '@trpc/server'
 import { descriptions } from '../descriptions'
 
 // only have one type in this edge collection right now
@@ -40,26 +39,17 @@ const schemaObj = schema['regulatory element to biosample']
 const router = new RouterEdges(schemaObj)
 
 async function conditionalBiosampleSearch (input: paramsFormatType): Promise<any[]> {
-  if (Object.keys(input).filter(item => !['term_id', 'term_name'].includes(item)).length === 0) {
-    throw new TRPCError({
-      code: 'BAD_REQUEST',
-      message: 'At least one gene property must be defined.'
-    })
-  }
-
   if (input.term_id !== undefined) {
-    input._to = `ontology_terms/${input.term_id}`
+    input._id = `ontology_terms/${input.term_id}`
     delete input.term_id
-    input.sort = '_key'
-    return await router.getEdgeObjects(input, '', input.verbose === 'true')
   }
 
-  return await router.getTargetEdgesByTokenTextSearch(input, 'term_name', input.verbose === 'true')
+  return await router.getSources(input, '_key', input.verbose === 'true', edgeQuery(input))
 }
 
 const biosamplesFromRegulatoryRegions = publicProcedure
   .meta({ openapi: { method: 'GET', path: '/regulatory_regions/biosamples', description: descriptions.regulatory_regions_biosamples } })
-  .input(regulatoryRegionsQueryFormat.omit({ organism: true, biochemical_activity: true }).merge(edgeTypes).merge((z.object({ verbose: z.enum(['true', 'false']).default('false') }))))
+  .input(regulatoryRegionsQueryFormat.omit({ organism: true, biochemical_activity: true, source: true }).merge(edgeTypes).merge((z.object({ verbose: z.enum(['true', 'false']).default('false') }))))
   .output(z.array(regulatoryRegionToBiosampleFormat))
   .query(async ({ input }) => await router.getTargetsWithEdgeFilter(input, '_key', '', input.verbose === 'true', edgeQuery(input)))
 
