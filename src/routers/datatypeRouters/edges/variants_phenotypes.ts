37a94d67244a15e643c407bc3b7dca57aabd335a
shengchd <shengchd@umich.edu>
2024-03-14 16:39:13 -0700

    fix long type and query by variant

--- src/routers/datatypeRouters/edges/variants_phenotypes.ts
+++ src/routers/datatypeRouters/edges/variants_phenotypes.ts
@@ -10,12 +10,13 @@ import { RouterFilterBy } from '../../genericRouters/routerFilterBy'
 import { descriptions } from '../descriptions'
 import { QUERY_LIMIT } from '../../../constants'
 import { db } from '../../../database'
+import { TRPCError } from '@trpc/server'
 
-// log10pvalue is not loaded as long...
+// should we denormalize trait under variants_phenotypes_studies?
 const variantPhenotypeFormat = z.object({
   'sequence variant': z.string().or(z.array(variantFormat)).optional(),
-  'ontology term': z.string().or(z.array(ontologyFormat)).optional(), // from variants_phenotypes collection
-  study: z.string().or(z.array(studyFormat)).optional(), // _to from variants_phenotypes_studies
+  'ontology term': z.string().or(z.array(ontologyFormat)).optional(),
+  study: z.string().or(z.array(studyFormat)).optional(),
   log10pvalue: z.number().nullable(),
   p_val: z.number().nullable(),
   p_val_exponent: z.number().nullable(),
@@ -26,7 +27,7 @@ const variantPhenotypeFormat = z.object({
   oddsr_ci_lower: z.number().nullable(),
   oddsr_ci_upper: z.number().nullable(),
   lead_chrom: z.string().nullable(),
-  lead_pos: z.string().nullable(),
+  lead_pos: z.number().nullable(),
   lead_ref: z.string().nullable(),
   lead_alt: z.string().nullable(),
   direction: z.string().nullable(),
@@ -47,7 +48,8 @@ const hyperEdgeSchema = schema['variant to phenotype to study']
 const routerEdge = new RouterEdges(edgeSchema, new RouterEdges(hyperEdgeSchema))
 // const studyRouter = new RouterFilterBy(studyObj)
 
-async function getHyperedgeFromVariantQuery (router: RouterEdges, input: paramsFormatType, hyperEdgeFilter = '', queryOptions = '', page: number = 0, verbose: boolean, sortBy: string = '_key'): Promise<any[]> {
+// Query for endpoint variants/phenotypes/, by p-value filter AND/OR variant query, (AND phenotype ontology id)
+async function getHyperedgeFromVariantQuery (router: RouterEdges, input: paramsFormatType, phenotypeFilter = '', hyperEdgeFilter = '', queryOptions = '', page: number = 0, verbose: boolean, sortBy: string = '_key'): Promise<any[]> {
   const variantCollection = variantSchema.db_collection_name as string
   const variantPhenotypeCollection = edgeSchema.db_collection_name as string
   const variantPhenotypeStudyCollection = hyperEdgeSchema.db_collection_name as string
@@ -61,25 +63,16 @@ async function getHyperedgeFromVariantQuery (router: RouterEdges, input: paramsF
       FILTER targetRecord._key == PARSE_IDENTIFIER(edgeRecord._to).key
       RETURN {${studyReturn.replaceAll('record', 'targetRecord')}}
   `
-  // add control if hyperEdgeFilter == '' or variant query == '' (should we allow that?)
-  let query
-  if (input.variant_id !== undefined) {
-    query = `
-      LET primaryEdges = (
-        FOR record IN ${variantPhenotypeCollection}
-        FILTER record._from == 'variants/${input.variant_id}'
-        RETURN record._id
-    )
 
-      FOR edgeRecord IN ${variantPhenotypeStudyCollection}
-      FILTER edgeRecord._from IN primaryEdges ${hyperEdgeFilter}
-      SORT '${sortBy}'
-      LIMIT ${page * QUERY_LIMIT}, ${QUERY_LIMIT}
-      RETURN {
-        ${router.secondaryRouter?.dbReturnStatements.replaceAll('record', 'edgeRecord') as string}
-      }
-    `
-  } else {
+  let query
+  if (phenotypeFilter !== '') {
+    phenotypeFilter = `and record._to == 'ontology_terms/${phenotypeFilter}'`
+  }
+
+  if (variantFilters !== '') {
+    if (hyperEdgeFilter !== '') {
+      hyperEdgeFilter = `and ${hyperEdgeFilter}`
+    }
     query = `
       LET primarySources = (
         FOR record IN ${variantCollection} ${queryOptions}
@@ -89,39 +82,52 @@ async function getHyperedgeFromVariantQuery (router: RouterEdges, input: paramsF
 
       LET primaryEdges = (
         FOR record IN ${variantPhenotypeCollection}
-        FILTER record._from IN primarySources
+        FILTER record._from IN primarySources ${phenotypeFilter}
         RETURN record._id
       )
 
       FOR edgeRecord IN ${variantPhenotypeStudyCollection}
-      FILTER edgeRecord._from IN primaryEdges ${hyperEdgeFilter}
+      FILTER edgeRecord._from IN primaryEdges ${hyperEdgeFilter.replaceAll('record', 'edgeRecord')}
       SORT '${sortBy}'
       LIMIT ${page * QUERY_LIMIT}, ${QUERY_LIMIT}
       RETURN {
         'study': ${verbose ? `(${verboseQuery})` : 'edgeRecord._to'},
         ${router.secondaryRouter?.dbReturnStatements.replaceAll('record', 'edgeRecord') as string}
       }
-  `
+      `
+  } else {
+    if (hyperEdgeFilter !== '') {
+      query = `
+        FOR record IN ${variantPhenotypeStudyCollection}
+        FILTER ${hyperEdgeFilter}
+        SORT '${sortBy}'
+        LIMIT ${page * QUERY_LIMIT}, ${QUERY_LIMIT}
+        RETURN {
+          'study': ${verbose ? `(${verboseQuery})` : 'record._to'},
+          ${router.secondaryRouter?.dbReturnStatements as string}
+        }
+      `
+    } else {
+      throw new TRPCError({
+        code: 'BAD_REQUEST',
+        message: 'At least one property must be defined.'
+      })
+    }
   }
+
   console.log(query)
   return await ((await db.query(query)).all())
-}
-
-async function getHyperEdgeFilters (input: paramsFormatType): Promise<string> {
-  const hyperEdgeFilters = []
-
-  if (input.log10pvalue !== undefined) {
-    hyperEdgeFilters.push(`${routerEdge.secondaryRouter?.getFilterStatements({ p_val: input.p_value }) as string}`)
-    delete input.log10pvalue
-  }
-
-  return hyperEdgeFilters.join(' and ')
 }
 
 async function variantSearch (input: paramsFormatType): Promise<any[]> {
   let queryOptions = ''
   if (input.region !== undefined) {
     queryOptions = 'OPTIONS { indexHint: "region", forceIndexHint: true }'
+  }
+
+  if (input.variant_id !== undefined) {
+    input._key = input.variant_id // or do we want to make _id query overwrites other queries?
+    delete input.variant_id
   }
 
   if (input.funseq_description !== undefined) {
@@ -135,9 +141,26 @@ async function variantSearch (input: paramsFormatType): Promise<any[]> {
     delete input.max_alt_freq
     delete input.source
   }
-
+  let phenotypeFilter = ''
+  if (input.phenotype_id !== undefined) {
+    phenotypeFilter = input.phenotype_id as string
+    delete input.phenotype_id
+  }
   const hyperEdgeFilters = await getHyperEdgeFilters(input)
-  return await getHyperedgeFromVariantQuery(routerEdge, preProcessRegionParam(input, 'pos'), hyperEdgeFilters, queryOptions, input.page as number, input.verbose === 'true')
+  // should preProcessRegionParam in input? it's also in filterStatements
+  return await getHyperedgeFromVariantQuery(routerEdge, preProcessRegionParam(input, 'pos'), phenotypeFilter, hyperEdgeFilters, queryOptions, input.page as number, input.verbose === 'true')
+}
+
+// parse p-value filter on hyperedges from input
+async function getHyperEdgeFilters (input: paramsFormatType): Promise<string> {
+  const hyperEdgeFilters = []
+
+  if (input.log10pvalue !== undefined) {
+    hyperEdgeFilters.push(`${routerEdge.secondaryRouter?.getFilterStatements({ log10pvalue: input.log10pvalue }) as string}`)
+    delete input.log10pvalue
+  }
+
+  return hyperEdgeFilters.join(' and ')
 }
 
 const variantsFromPhenotypes = publicProcedure
@@ -148,7 +171,7 @@ const variantsFromPhenotypes = publicProcedure
 
 const phenotypesFromVariants = publicProcedure
   .meta({ openapi: { method: 'GET', path: '/variants/phenotypes', description: descriptions.variants_phenotypes } })
-  .input(variantsQueryFormat.merge(z.object({ log10pvalue: z.string().trim().optional(), verbose: z.enum(['true', 'false']).default('false') })))
+  .input(variantsQueryFormat.merge(z.object({ phenotype_id: z.string().trim().optional(), log10pvalue: z.string().trim().optional(), verbose: z.enum(['true', 'false']).default('false') })))
   .output(z.array(variantPhenotypeFormat))
   .query(async ({ input }) => await variantSearch(input))
 
