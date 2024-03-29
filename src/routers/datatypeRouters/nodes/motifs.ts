c6d3652a69e9af7f0aa7a50ef9ecd25265c68dd7
Pedro Assis <pedroh2306@gmail.com>
2024-03-29 12:00:58 -1000

    refactoring motifs and adding limit param

--- src/routers/datatypeRouters/nodes/motifs.ts
+++ src/routers/datatypeRouters/nodes/motifs.ts
@@ -1,14 +1,18 @@
 import { z } from 'zod'
+import { db } from '../../../database'
+import { QUERY_LIMIT } from '../../../constants'
 import { publicProcedure } from '../../../trpc'
 import { loadSchemaConfig } from '../../genericRouters/genericRouters'
-import { RouterFilterBy } from '../../genericRouters/routerFilterBy'
-import { paramsFormatType } from '../_helpers'
+import { getDBReturnStatements, getFilterStatements, paramsFormatType } from '../_helpers'
 import { descriptions } from '../descriptions'
 
+const MAX_PAGE_SIZE = 500
+
 const schema = loadSchemaConfig()
+const motifSchema = schema.motif
 
 export const motifsQueryFormat = z.object({
-  tf_name: z.string().trim(),
+  tf_name: z.string().trim().optional(),
   source: z.string().trim().optional(),
   page: z.number().default(0)
 })
@@ -22,20 +26,39 @@ export const motifFormat = z.object({
   source_url: z.string()
 })
 
-const schemaObj = schema.motif
-const router = new RouterFilterBy(schemaObj)
+async function motifSearch (input: paramsFormatType): Promise<any[]> {
+  if (input.tf_name !== undefined) {
+    input.tf_name = (input.tf_name as string).toUpperCase()
+  }
 
-function preProcessInput (input: paramsFormatType): paramsFormatType {
-  input.tf_name = (input.tf_name as string).toUpperCase()
-  delete input.name
-  return input
+  let limit = QUERY_LIMIT
+  if (input.limit !== undefined) {
+    limit = (input.limit as number <= MAX_PAGE_SIZE) ? input.limit as number : MAX_PAGE_SIZE
+    delete input.limit
+  }
+
+  let filterBy = ''
+  const filterSts = getFilterStatements(motifSchema, input)
+  if (filterSts !== '') {
+    filterBy = `FILTER ${filterSts}`
+  }
+
+  const query = `
+    FOR record IN ${motifSchema.db_collection_name}
+    ${filterBy}
+    SORT record._key
+    LIMIT ${input.page as number * limit}, ${limit}
+    RETURN { ${getDBReturnStatements(motifSchema)} }
+  `
+
+  return await (await db.query(query)).all()
 }
 
 const motifs = publicProcedure
-  .meta({ openapi: { method: 'GET', path: `/${router.apiName}`, description: descriptions.motifs } })
-  .input(motifsQueryFormat)
+  .meta({ openapi: { method: 'GET', path: `/motifs`, description: descriptions.motifs } })
+  .input(motifsQueryFormat.merge(z.object({ limit: z.number().optional() })))
   .output(z.array(motifFormat))
-  .query(async ({ input }) => await router.getObjects(preProcessInput(input)))
+  .query(async ({ input }) => await motifSearch(input))
 
 export const motifsRouters = {
   motifs
