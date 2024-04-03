b650cbc5b51d35f5ae48921dbddfbf0d53d47280
Pedro Assis <pedroh2306@gmail.com>
2024-04-02 22:48:13 -0300

    name should contain hgvsp

--- data/adapters/dbSNFP_adapter.py
+++ data/adapters/dbSNFP_adapter.py
@@ -152,7 +152,7 @@ class DbSNFPAdapter(Adapter):
                 else:
                     to_json = {
                         '_key': key,
-                        'name': (data(12) or '') + '_' + (data(22) or ''),
+                        'name': (data(12) or '') + '_' + (data(23) or ''),
                         'ref': data(4),
                         'alt': data(5),
                         'aapos:long': long_data(11),  # 1-based
