7e045f45eb73dc818d75559a005e8f48a3040272
Pedro Assis <pedroh2306@gmail.com>
2024-04-02 15:46:41 -1000

    refactoring ontology_terms relationship endpoints

--- src/routers/datatypeRouters/edges/ontology_terms_ontology_terms.ts
+++ src/routers/datatypeRouters/edges/ontology_terms_ontology_terms.ts
@@ -1,17 +1,18 @@
 import { z } from 'zod'
+import { db } from '../../../database'
+import { Edge, PathArangoDB, Paths, QUERY_LIMIT } from '../../../constants'
 import { publicProcedure } from '../../../trpc'
 import { loadSchemaConfig } from '../../genericRouters/genericRouters'
-import { RouterTransitiveClosure } from '../../genericRouters/routerTransitiveClosure'
 import { descriptions } from '../descriptions'
-import { RouterEdges } from '../../genericRouters/routerEdges'
 import { ontologyFormat } from '../nodes/ontologies'
+import { getDBReturnStatements, paramsFormatType } from '../_helpers'
+
+const MAX_PAGE_SIZE = 500
 
 const schema = loadSchemaConfig()
 
 const edgeSchemaObj = schema['ontology relationship']
-
-const router = new RouterEdges(edgeSchemaObj)
-const routerClosure = new RouterTransitiveClosure(edgeSchemaObj, 'ontology_terms')
+const ontologyTermSchema = schema['ontology term']
 
 const ontologyRelativeFormat = z.object({
   term: ontologyFormat,
@@ -20,26 +21,104 @@ const ontologyRelativeFormat = z.object({
 
 const ontologyRelativeQueryFormat = z.object({
   ontology_term_id: z.string().trim(),
-  page: z.number().default(0)
+  page: z.number().default(0),
+  limit: z.number().optional()
 })
+
+async function getChildrenParents (input: paramsFormatType, opt: string): Promise<any[]> {
+  const id = `${ontologyTermSchema.db_collection_name}/${decodeURIComponent(input.ontology_term_id as string)}`
+
+  let limit = QUERY_LIMIT
+  if (input.limit !== undefined) {
+    limit = (input.limit as number <= MAX_PAGE_SIZE) ? input.limit as number : MAX_PAGE_SIZE
+    delete input.limit
+  }
+
+  const query = `
+    FOR record IN ${edgeSchemaObj.db_collection_name}
+      LET details = (
+        FOR otherRecord IN ${ontologyTermSchema.db_collection_name}
+        FILTER otherRecord._id == record.${opt === 'children' ? '_from' : '_to'}
+        RETURN {${getDBReturnStatements(ontologyTermSchema).replaceAll('record', 'otherRecord')}}
+      )[0]
+
+      FILTER record.${opt === 'children' ? '_to' : '_from'} == '${id}' && details != null && record.type == 'subclass'
+      SORT record._key
+      LIMIT ${input.page as number * limit}, ${limit}
+
+      RETURN {
+        'term': details,
+        'relationship_type': record.type || 'null'
+      }
+  `
+
+  return await (await db.query(query)).all()
+}
+
+async function getPaths (from: string, to: string, fields: string[]): Promise<any> {
+  const query = `
+    FOR fromObj IN ${ontologyTermSchema.db_collection_name}
+      FILTER fromObj._key == '${decodeURIComponent(from)}'
+
+    FOR toObj IN ${ontologyTermSchema.db_collection_name}
+      FILTER toObj._key == '${decodeURIComponent(to)}'
+
+    FOR path IN ANY ALL_SHORTEST_PATHS
+      fromObj TO toObj
+      ${edgeSchemaObj.db_collection_name}
+      RETURN path
+  `
+
+  const paths = await (await db.query(query)).all() as PathArangoDB[]
+
+  const totalVertices: Record<string, any> = {}
+  const edgesPaths: Edge[][] = []
+
+  paths.forEach(path => {
+    path.vertices.forEach(vertix => {
+      const filteredObject: Record<string, any> = {}
+      fields.forEach((key) => {
+        filteredObject[key] = vertix[key]
+      })
+
+      totalVertices[vertix._key] = filteredObject
+    })
+    const edges: Edge[] = []
+    path.edges.forEach(edge => {
+      edges.push({
+        from: edge._from.split('/')[1],
+        to: edge._to.split('/')[1],
+        type: edge.type
+      })
+    })
+    edgesPaths.push(edges)
+  })
+
+  const returnValue: Paths = {
+    vertices: totalVertices,
+    paths: edgesPaths
+  }
+
+  return returnValue
+}
 
 const ontologyTermChildren = publicProcedure
   .meta({ openapi: { method: 'GET', path: '/ontology_terms/{ontology_term_id}/children', description: descriptions.ontology_terms_children } })
   .input(ontologyRelativeQueryFormat)
   .output(z.array(ontologyRelativeFormat.optional()))
-  .query(async ({ input }) => await router.getChildrenParents(input.ontology_term_id, 'children', '_key', input.page))
+  .query(async ({ input }) => await getChildrenParents(input, 'children'))
 
 const ontologyTermParents = publicProcedure
   .meta({ openapi: { method: 'GET', path: '/ontology_terms/{ontology_term_id}/parents', description: descriptions.ontology_terms_parents } })
   .input(ontologyRelativeQueryFormat)
   .output(z.array(ontologyRelativeFormat.optional()))
-  .query(async ({ input }) => await router.getChildrenParents(input.ontology_term_id, 'parents', '_key', input.page))
+  .query(async ({ input }) => await getChildrenParents(input, 'parents'))
 
 const ontologyTermTransitiveClosure = publicProcedure
   .meta({ openapi: { method: 'GET', path: '/ontology_terms/{ontology_term_id_start}/transitive_closure/{ontology_term_id_end}', description: descriptions.ontology_terms_transitive_closure } })
   .input(z.object({ ontology_term_id_start: z.string().trim(), ontology_term_id_end: z.string().trim() }))
   .output(z.any())
-  .query(async ({ input }) => await routerClosure.getPaths(input.ontology_term_id_start, input.ontology_term_id_end, Object.keys(ontologyFormat.shape)))
+  .query(async ({ input }) => await getPaths(input.ontology_term_id_start, input.ontology_term_id_end, Object.keys(ontologyFormat.shape)))
 
 export const ontologyTermsEdgeRouters = {
   ontologyTermChildren,
