fa799c8b3cce3f9dba6207728f01903db7acd072
shengchd <shengchd@umich.edu>
2024-04-08 16:03:13 -0700

    fix bug on schema

--- data/adapters/AFGR_eqtl_adapter.py
+++ data/adapters/AFGR_eqtl_adapter.py
@@ -47,6 +47,7 @@ class AFGREQtl(Adapter):
                     # The three numeric values are not loaded as long data type somehow, though in schema it's labeled as int
                     # Manually changed data type from double to long in header file before importing into Arangodb
                     'log10pvalue': float(row[8]),  # MAX=616
+                    'p_value': float(row[9]),
                     'effect_size': float(row[10]),
                     'label': 'eQTL',
                     'source': AFGREQtl.SOURCE,
