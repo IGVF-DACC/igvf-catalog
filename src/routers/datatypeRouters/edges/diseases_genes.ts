f96486e87af8b9ababd9d5aec8fb28dbec380b55
Pedro Assis <pedroh2306@gmail.com>
2024-04-10 16:32:45 -1000

    diseases genes refactoring

--- src/routers/datatypeRouters/edges/diseases_genes.ts
+++ src/routers/datatypeRouters/edges/diseases_genes.ts
@@ -1,12 +1,15 @@
 import { z } from 'zod'
+import { db } from '../../../database'
+import { QUERY_LIMIT } from '../../../constants'
 import { publicProcedure } from '../../../trpc'
 import { loadSchemaConfig } from '../../genericRouters/genericRouters'
-import { RouterEdges } from '../../genericRouters/routerEdges'
 import { geneFormat, genesQueryFormat } from '../nodes/genes'
 import { ontologyFormat } from '../nodes/ontologies'
-import { paramsFormatType } from '../_helpers'
+import { getDBReturnStatements, getFilterStatements, paramsFormatType, preProcessRegionParam } from '../_helpers'
 import { TRPCError } from '@trpc/server'
 import { descriptions } from '../descriptions'
+
+const MAX_PAGE_SIZE = 100
 
 const associationTypes = z.object({
   association_type: z.enum([
@@ -24,6 +27,9 @@ const associationTypes = z.object({
 })
 
 const schema = loadSchemaConfig()
+const diseaseToGeneSchema = schema['disease to gene']
+const diseaseSchema = schema['ontology term']
+const geneSchema = schema.gene
 
 const diseasesToGenesFormat = z.object({
   pmid: z.array(z.string()).optional(),
@@ -34,11 +40,8 @@ const diseasesToGenesFormat = z.object({
   source: z.string().optional(),
   source_url: z.string().optional(),
   gene: z.string().or(geneFormat).optional(),
-  'ontology term': z.string().or(ontologyFormat).optional()
+  'disease': z.string().or(ontologyFormat).optional()
 })
-
-const schemaObj = schema['disease to gene']
-const router = new RouterEdges(schemaObj)
 
 function edgeQuery (input: paramsFormatType): string {
   const query = []
@@ -63,7 +66,7 @@ function edgeQuery (input: paramsFormatType): string {
   return query.join('and ')
 }
 
-async function conditionalDiseaseSearch (input: paramsFormatType): Promise<any[]> {
+async function genesFromDiseaseSearch (input: paramsFormatType): Promise<any[]> {
   if (Object.keys(input).filter(item => !['disease_id', 'term_name'].includes(item)).length === 0) {
     throw new TRPCError({
       code: 'BAD_REQUEST',
@@ -71,23 +74,115 @@ async function conditionalDiseaseSearch (input: paramsFormatType): Promise<any[]
     })
   }
 
+  let limit = QUERY_LIMIT
+  if (input.limit !== undefined) {
+    limit = (input.limit as number <= MAX_PAGE_SIZE) ? input.limit as number : MAX_PAGE_SIZE
+    delete input.limit
+  }
+
+  const verbose = input.verbose === 'true'
+
   if (input.disease_id !== undefined) {
     input._from = `ontology_terms/${input.disease_id}`
     delete input.disease_id
-    input.sort = '_key'
-    return await router.getEdgeObjects(input, '', input.verbose === 'true')
+
+    const sourceQuery = `FOR otherRecord IN ${diseaseSchema.db_collection_name}
+      FILTER otherRecord._id == record._from
+      RETURN {${getDBReturnStatements(diseaseSchema).replaceAll('record', 'otherRecord')}}
+    `
+
+    const targetQuery = `
+      FOR otherRecord IN ${geneSchema.db_collection_name}
+      FILTER otherRecord._id == record._to
+      RETURN {${getDBReturnStatements(geneSchema).replaceAll('record', 'otherRecord')}}
+    `
+
+    const sourceReturn = `'disease': ${verbose ? `(${sourceQuery})[0]` : 'record._from'},`
+    const targetReturn = `'gene': ${verbose ? `(${targetQuery})[0]` : 'record._to'},`
+
+    const query = `
+      FOR record IN ${diseaseToGeneSchema.db_collection_name}
+      FILTER ${getFilterStatements(diseaseToGeneSchema, input)}
+      SORT record._key
+      LIMIT ${input.page as number * limit}, ${limit}
+      RETURN { ${sourceReturn + targetReturn + getDBReturnStatements(diseaseToGeneSchema)} }
+    `
+
+    return await (await db.query(query)).all()
   }
 
-  return await router.getTargetEdgesByTokenTextSearch(input, 'term_name', input.verbose === 'true')
+  const searchTerm = input.term_name as string
+  const searchViewName = `${diseaseToGeneSchema.db_collection_name}_fuzzy_search_alias`
+
+  delete input.term_name
+
+  const verboseQuery = `
+    FOR otherRecord IN ${geneSchema.db_collection_name}
+    FILTER otherRecord._key == PARSE_IDENTIFIER(record._to).key
+    RETURN {${getDBReturnStatements(geneSchema).replaceAll('record', 'otherRecord')}}
+  `
+
+  let filters = getFilterStatements(diseaseToGeneSchema, input)
+  if (filters !== '') {
+    filters = `FILTER ${filters}`
+  }
+
+  const query = `
+    FOR record IN ${searchViewName}
+      SEARCH TOKENS("${decodeURIComponent(searchTerm)}", "text_en_no_stem") ALL in record.term_name
+      SORT BM25(record) DESC
+      ${filters}
+      LIMIT ${input.page as number * limit}, ${limit}
+      RETURN {
+        ${getDBReturnStatements(diseaseToGeneSchema)},
+        'gene': ${verbose ? `(${verboseQuery})[0]` : 'record._to'}
+      }
+  `
+
+  return await (await db.query(query)).all()
 }
 
-async function conditionalGeneSearch (input: paramsFormatType): Promise<any[]> {
+async function diseasesFromGeneSearch (input: paramsFormatType): Promise<any[]> {
   if (input.gene_id !== undefined) {
     input._id = `genes/${input.gene_id}`
     delete input.gene_id
   }
 
-  return await router.getSources(input, '_key', input.verbose === 'true', edgeQuery(input))
+  let limit = QUERY_LIMIT
+  if (input.limit !== undefined) {
+    limit = (input.limit as number <= MAX_PAGE_SIZE) ? input.limit as number : MAX_PAGE_SIZE
+    delete input.limit
+  }
+
+  let customFilter = edgeQuery(input)
+  if (customFilter !== '') {
+    customFilter = `and ${customFilter}`
+  }
+
+  const verboseQuery = `
+    FOR otherRecord IN ${diseaseSchema.db_collection_name}
+    FILTER otherRecord._key == PARSE_IDENTIFIER(record._from).key
+    RETURN {${getDBReturnStatements(diseaseSchema).replaceAll('record', 'otherRecord')}}
+  `
+
+  const query = `
+    LET targets = (
+      FOR record IN ${geneSchema.db_collection_name}
+      FILTER ${getFilterStatements(geneSchema, preProcessRegionParam(input))}
+      RETURN record._id
+    )
+
+    FOR record IN ${diseaseToGeneSchema.db_collection_name}
+      FILTER record._to IN targets ${customFilter}
+      SORT record._key
+      LIMIT ${input.page as number * limit}, ${limit}
+      RETURN {
+        'disease': ${input.verbose === 'true' ? `(${verboseQuery})[0]` : 'record._from'},
+        ${getDBReturnStatements(diseaseToGeneSchema)}
+      }
+  `
+
+  return await (await db.query(query)).all()
 }
 
 const geneQuery = genesQueryFormat.omit({
@@ -97,23 +192,33 @@ const geneQuery = genesQueryFormat.omit({
   gene_name: z.string().trim().optional(),
   source: z.string().trim().optional(),
   page: z.number().default(0),
-  verbose: z.enum(['true', 'false']).default('false')
+  verbose: z.enum(['true', 'false']).default('false'),
+  limit: z.number().optional()
 })).merge(associationTypes).transform(({gene_name, ...rest}) => ({
   name: gene_name,
   ...rest
+}))
+
+const diseaseQuery = associationTypes.merge(z.object({
+  disease_id: z.string().trim().optional(),
+  term_name: z.string().trim().optional(),
+  source: z.string().trim().optional(),
+  page: z.number().default(0),
+  verbose: z.enum(['true', 'false']).default('false'),
+  limit: z.number().optional()
 }))
 
 const diseasesFromGenes = publicProcedure
   .meta({ openapi: { method: 'GET', path: '/genes/diseases', description: descriptions.genes_diseases } })
   .input(geneQuery)
   .output(z.array(diseasesToGenesFormat))
-  .query(async ({ input }) => await conditionalGeneSearch(input))
+  .query(async ({ input }) => await diseasesFromGeneSearch(input))
 
 const genesFromDiseases = publicProcedure
   .meta({ openapi: { method: 'GET', path: '/diseases/genes', description: descriptions.diseases_genes } })
-  .input(associationTypes.merge(z.object({ disease_id: z.string().trim().optional(), term_name: z.string().trim().optional(), source: z.string().trim().optional(), page: z.number().default(0), verbose: z.enum(['true', 'false']).default('false') })))
+  .input(diseaseQuery)
   .output(z.array(diseasesToGenesFormat))
-  .query(async ({ input }) => await conditionalDiseaseSearch(input))
+  .query(async ({ input }) => await genesFromDiseaseSearch(input))
 
 export const diseasesGenesRouters = {
   genesFromDiseases,
