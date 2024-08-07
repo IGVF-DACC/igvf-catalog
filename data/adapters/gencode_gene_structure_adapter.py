d069f82e31d8fbfbdbb3c23f76499b4b24fb862c
shengchd <shengchd@umich.edu>
2024-08-07 11:54:11 -0700

    DSERV-583-gene-structure-gencode

---
+++ data/adapters/gencode_gene_structure_adapter.py
@@ -0,0 +1,118 @@
+from adapters import Adapter
+import gzip
+import json
+import os
+from db.arango_db import ArangoDB
+
+# Example genocde gtf input file:
+# ##description: evidence-based annotation of the human genome (GRCh38), version 43 (Ensembl 109)
+# ##provider: GENCODE
+# ##contact: gencode-help@ebi.ac.uk
+# ##format: gtf
+# ##date: 2022-11-29
+# chr1	HAVANA	gene	11869	14409	.	+	.	gene_id "ENSG00000290825.1"; gene_type "lncRNA"; gene_name "DDX11L2"; level 2; tag "overlaps_pseudogene";
+# chr1	HAVANA	transcript	11869	14409	.	+	.	gene_id "ENSG00000290825.1"; transcript_id "ENST00000456328.2"; gene_type "lncRNA"; gene_name "DDX11L2"; transcript_type "lncRNA"; transcript_name "DDX11L2-202"; level 2; transcript_support_level "1"; tag "basic"; tag "Ensembl_canonical"; havana_transcript "OTTHUMT00000362751.1";
+# chr1	HAVANA	exon	11869	12227	.	+	.	gene_id "ENSG00000290825.1"; transcript_id "ENST00000456328.2"; gene_type "lncRNA"; gene_name "DDX11L2"; transcript_type "lncRNA"; transcript_name "DDX11L2-202"; exon_number 1; exon_id "ENSE00002234944.1"; level 2; transcript_support_level "1"; tag "basic"; tag "Ensembl_canonical"; havana_transcript "OTTHUMT00000362751.1";
+# chr1	HAVANA	exon	12613	12721	.	+	.	gene_id "ENSG00000290825.1"; transcript_id "ENST00000456328.2"; gene_type "lncRNA"; gene_name "DDX11L2"; transcript_type "lncRNA"; transcript_name "DDX11L2-202"; exon_number 2; exon_id "ENSE00003582793.1"; level 2; transcript_support_level "1"; tag "basic"; tag "Ensembl_canonical"; havana_transcript "OTTHUMT00000362751.1";
+
+# Column three has the gene structure info we want to load. Each exon can have substructures of CDS, UTR, start_condon, and stop_condon, which will have the same exon_id.
+
+
+class GencodeStructure(Adapter):
+    ALLOWED_KEYS = ['gene_id', 'gene_name',
+                    'transcript_id', 'exon_number', 'exon_id']
+
+    # should strand be added to transcripts collection?
+    INDEX = {'chr': 0, 'type': 2, 'coord_start': 3,
+             'coord_end': 4, 'strand': 6, 'info': 8}
+
+    STRUCTURE_TYPES = ['CDS', 'UTR', 'exon', 'start_codon',
+                       'stop_codon']  # todo: check Selenocysteine
+
+    ALLOWED_LABELS = [
+        'gene_structure',  # human
+        'mm_gene_structure'  # mouse
+    ]
+
+    OUTPUT_FOLDER = './parsed-data'
+
+    def __init__(self, filepath=None, chr='all', label='gene_structure', dry_run=False):
+        if label not in GencodeStructure.ALLOWED_LABELS:
+            raise ValueError('Ivalid label. Allowed values: ' +
+                             ','.join(GencodeStructure.ALLOWED_LABELS))
+        self.filepath = filepath
+        self.chr = chr
+        self.label = label
+        self.dry_run = dry_run
+        self.organism = 'Homo sapiens'
+        self.output_filepath = '{}/{}.json'.format(
+            GencodeStructure.OUTPUT_FOLDER,
+            self.label
+        )
+        self.SKIP_BIOCYPHER = True
+
+        if self.label == 'gene_structure':
+            self.version = 'v43'
+            self.source_url = 'https://www.gencodegenes.org/human/'
+        else:
+            self.organism = 'Mus musculus'
+            self.version = 'vM33'
+            self.source_url = 'https://www.gencodegenes.org/mouse/'
+
+        super(GencodeStructure, self).__init__()
+
+    def parse_info_metadata(self, info):
+        parsed_info = {}
+        for key, value in zip(info, info[1:]):
+            if key in GencodeStructure.ALLOWED_KEYS:
+                parsed_info[key] = value.replace('"', '').replace(';', '')
+        return parsed_info
+
+    def process_file(self):
+        parsed_data_file = open(self.output_filepath, 'w')
+
+        for line in open(self.filepath, 'r'):
+            if line.startswith('#'):
+                continue
+            split_line = line.strip().split()
+            type = split_line[GencodeStructure.INDEX['type']]
+
+            if type in GencodeStructure.STRUCTURE_TYPES:
+                info = self.parse_info_metadata(
+                    split_line[GencodeStructure.INDEX['info']:])
+
+                to_json = {
+                    '_key': info['exon_id'] + type,
+                    # dropped gene_name since it's part of the transcript_name
+                    'name': info['transcript_name'] + '_exon_ ' + info['exon_number'] + '_' + type,
+                    'chr': split_line[GencodeStructure.INDEX['chr']],
+                    # the gtf file format is [1-based,1-based], needs to convert to BED format [0-based,1-based]
+                    'start:long': int(split_line[GencodeStructure.INDEX['coord_start']]) - 1,
+                    'end:long': int(split_line[GencodeStructure.INDEX['coord_end']]),
+                    'strand': split_line[GencodeStructure.INDEX['strand']],
+                    'type': type,
+                    'gene_name': info['gene_name'],
+                    'transcript_name': info['transcript_name'],
+                    # intron will be 1_2
+                    'exon_number': str(info['exon_number']),
+                    'exon_id': info['exon_id'],
+                    'source': 'GENCODE',
+                    'version': self.version,
+                    'source_url': self.source_url,
+                    'organism': self.organism
+                }
+
+                json.dump(to_json, parsed_data_file)
+                parsed_data_file.write('\n')
+
+        parsed_data_file.close()
+        self.save_to_arango()
+
+    def arangodb(self):
+        return ArangoDB().generate_json_import_statement(self.output_filepath, self.collection)
+
+    def save_to_arango(self):
+        if self.dry_run:
+            print(self.arangodb()[0])
+        else:
+            os.system(self.arangodb()[0])
