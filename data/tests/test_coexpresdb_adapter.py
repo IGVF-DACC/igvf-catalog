847b77f4a46ee2e154bfec21729517f0b81c8b8c
Otto Jolanki <ojolanki@stanford.edu>
2024-09-16 17:05:09 -0700

    coexpresdb tests

---
+++ data/tests/test_coexpresdb_adapter.py
@@ -0,0 +1,44 @@
+import json
+
+from adapters.coxpresdb_adapter import Coxpresdb
+from adapters.writer import SpyWriter
+
+
+def test_coxpresdb_adapter():
+    writer = SpyWriter()
+    adapter = Coxpresdb(filepath='./samples/coxpresdb/1', writer=writer)
+    adapter.process_file()
+
+    assert len(writer.contents) > 0
+    first_item = json.loads(writer.contents[0])
+
+    assert '_key' in first_item
+    assert '_from' in first_item
+    assert '_to' in first_item
+    assert 'z_score' in first_item
+    assert first_item['source'] == 'CoXPresdb'
+    assert first_item['source_url'] == 'https://coxpresdb.jp/'
+    assert first_item['name'] == 'coexpressed with'
+    assert first_item['inverse_name'] == 'coexpressed with'
+    assert first_item['associated process'] == 'ontology_terms/GO_0010467'
+
+
+def test_coxpresdb_adapter_z_score_filter():
+    writer = SpyWriter()
+    adapter = Coxpresdb(filepath='./samples/coxpresdb/1', writer=writer)
+    adapter.process_file()
+
+    for item in writer.contents:
+        if item.startswith('{'):
+            data = json.loads(item)
+            assert abs(float(data['z_score'])) >= 3
+
+
+def test_coxpresdb_adapter_initialization():
+    adapter = Coxpresdb(filepath='foobarbaz')
+    assert adapter.file_path == 'foobarbaz'
+    assert adapter.dataset == 'coxpresdb'
+    assert adapter.label == 'coxpresdb'
+    assert adapter.source == 'CoXPresdb'
+    assert adapter.source_url == 'https://coxpresdb.jp/'
+    assert adapter.type == 'edge'
