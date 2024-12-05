0416edfbe2c973cf7e166f590f5e70dd04239200
Mingjie Li <mingjiecn@gmail.com>
2024-12-05 17:07:04 -0600

    update index script

--- data/catalog_indexes_setup.py
+++ data/catalog_indexes_setup.py
@@ -1,30 +1,83 @@
-import os
 import argparse
-from active_adapters import ADAPTERS
+import yaml
 
 from db.arango_db import ArangoDB
+
+CONFIG_PATH = './index-config.yaml'
+index_config = yaml.safe_load(open(CONFIG_PATH, 'r'))
+collections_in_config = list(index_config.keys())
 
 parser = argparse.ArgumentParser(
     prog='IGVF Catalog DB Index Setup',
     description='Creates indexes in the development.json IGVF Catalog ArangoDB collections'
 )
 
-parser.add_argument('-a', '--adapter', nargs='*',
-                    help='Creates collection indexes in the development.json DB', choices=ADAPTERS.keys())
+parser.add_argument('-c', '--collection', nargs='*',
+                    help='Creates collection indexes in the development.json DB', choices=collections_in_config)
 
 args = parser.parse_args()
-adapters = args.adapter or ADAPTERS.keys()
+collections_to_index = args.collection or collections_in_config
 
-for a in adapters:
-    adapter = ADAPTERS[a]
 
-    if adapter.has_indexes():
-        print(('{} - creating...').format(a))
+def create_indexes(indexes, collection):
+    for index in indexes:
+        if indexes[index]['type'] == 'inverted':
+            continue  # it's already handled by aliases
 
-        try:
-            adapter.create_indexes()
-            adapter.create_aliases()
-        except Exception as error:
-            print('{} - index creation failed. {}'.format(a, error))
-    else:
-        print('{} - skipping... there are no indexes registered in schema_config.yml.'.format(a))
+        fields_list = indexes[index]['fields']
+
+        for fields in fields_list:
+            fields = [f.strip() for f in fields.split(',')]
+            ArangoDB().create_index(
+                collection,
+                indexes[index]['type'],
+                fields
+            )
+
+
+def create_aliases_arango(fields_list, index_name, analyzer, view_name):
+    fields = [f.strip() for f in fields_list.split(',')]
+
+    ArangoDB().create_index(
+        collection,
+        'inverted',
+        fields,
+        name=index_name,
+        opts={'analyzer': analyzer}
+    )
+
+    ArangoDB().create_view(
+        view_name,
+        'search-alias',
+        collection,
+        index_name
+    )
+
+
+def create_aliases(indexes, collection):
+    if indexes.get('fuzzy_text_search'):
+        fields_list = indexes['fuzzy_text_search']
+        index_name = '{}_fuzzy_search'.format(collection)
+        view_name = '{}_fuzzy_search_alias'.format(collection)
+        analyzer = 'text_en_no_stem'
+        create_aliases_arango(
+            fields_list, index_name, analyzer, view_name)
+
+    if indexes.get('delimiter_text_search'):
+        fields_list = indexes['delimiter_text_search']
+        index_name = '{}_delimiter_search'.format(collection)
+        view_name = '{}_delimiter_search_alias'.format(collection)
+        analyzer = 'text_delimiter'
+        create_aliases_arango(
+            fields_list, index_name, analyzer, view_name)
+
+
+for collection in collections_to_index:
+    indexes = index_config[collection]
+    print(('{} - creating...').format(collection))
+
+    try:
+        create_indexes(indexes, collection)
+        create_aliases(indexes, collection)
+    except Exception as error:
+        print('{} - index creation failed. {}'.format(collection, error))
