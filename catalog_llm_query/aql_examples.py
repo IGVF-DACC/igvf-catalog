c5bf7f13e9fa9c47ae422440c718c4d7a845f4b1
Mingjie Li <mingjiecn@gmail.com>
2025-03-05 15:41:58 -0600

    add more aql samples

--- catalog_llm_query/aql_examples.py
+++ catalog_llm_query/aql_examples.py
@@ -30,4 +30,106 @@ AQL_EXAMPLES = """
         FOR t IN transcripts_proteins
             FILTER t._to == p._id
             RETURN t
+
+    # what genomic elements overlap rs1047055?
+    LET variant = FIRST(
+    FOR v IN variants
+        FILTER v.rsid == "rs1047055"
+        RETURN { chr: v.chr, pos: v.pos}
+    )
+    FOR g IN genomic_elements
+        FILTER g.chr == variant.chr
+        FILTER g.start <= variant.pos AND g.end > variant.pos
+        RETURN DISTINCT g
+
+    # For eQTL data, what variants are active in tissue that is part of heart. Sort by p-value.
+    LET heartTerms = (
+    FOR t IN ontology_terms
+        FILTER t.name == "heart"
+        FOR v, e IN 1..10 INBOUND t ontology_terms_ontology_terms
+            FILTER e.name IN ["part of"]
+            RETURN DISTINCT v
+    )
+    FOR tgt IN heartTerms
+        FOR vgt IN variants_genes_terms
+            FILTER vgt._to == tgt._id
+            FOR vg IN variants_genes
+                FILTER vg._id == vgt._from
+                limit 5
+                RETURN DISTINCT { term: tgt, variant: vg._from, gene: DOCUMENT(vg._to)}
+
+    # Find the genomic elements that are linked to PP1F (ENSG0000ENSG00000187642), with score >0.85
+    WITH genomic_elements, genomic_elements_genes
+    FOR gene IN genomic_elements_genes
+        FILTER gene._to == "genes/ENSG00000187642" AND gene.score > 0.85
+        FOR element IN genomic_elements
+            FILTER element._id == gene._from
+            RETURN element
+
+    # Find the top 5 eQTLs in ontology term named heart left ventricle, sorted by p_value.
+    LET heartTerms = (
+    FOR t IN ontology_terms
+        FILTER t.name == "heart left ventricle"
+        RETURN t
+    )
+    FOR vgt IN variants_genes_terms
+        FILTER vgt._to in heartTerms[*]._id
+    FOR vg in variants_genes
+        FILTER vgt._from == vg._id
+        FILTER vg.label == "eQTL"
+        SORT vg.`p_value` ASC
+        LIMIT 5
+        FOR v in variants
+        FILTER v._id == vg._from
+            FOR g in genes
+            FILTER g._id == vg._to
+    RETURN {'p-val': vg.`p_value`,
+            'biosample': vg.biological_context,
+            'gene': g.gene_name,
+            'variant': v.spdi}
+
+    # For variant with rsID rs32475, find the genomic elements that it is contained in.
+    LET variant = FIRST(
+        FOR v IN variants
+            FILTER "rs309428" in v.rsid
+            RETURN v
+    )
+    FOR g IN genomic_elements
+        FILTER g.chr == variant.chr
+        FILTER g.start <= variant.pos AND g.end > variant.pos
+        RETURN g
+
+    # Find the top 5 genes that are co-expressed with gene ENSG00000261221, sorted by logit_score.
+    FOR gg in genes_genes
+        FILTER gg._from == 'genes/ENSG00000261221'
+        SORT gg.`logit_score` ASC
+        limit 5
+        RETURN gg
+
+    # Find genes that are linked to variant with SPDI NC_000005.10:173860847:G:A.
+    LET variant = FIRST(
+        FOR v IN variants
+            FILTER v.spdi == 'NC_000005.10:173860847:G:A'
+            RETURN v
+    )
+    FOR vg IN variants_genes
+        FILTER vg._from == variant._id
+        FOR gene IN genes
+            FILTER gene._id == vg._to
+            RETURN gene
+
+    # Is variant with rsID rs875741 a caQTL?
+    FOR v in variants
+        FILTER "rs875741" in v.rsid
+    FOR vg IN variants_genomic_elements
+        FILTER vg._from == v._id
+        return vg
+
+    # find 5 variants linked to gene ENSG00000188290.
+    FOR vg IN variants_genes
+        FILTER vg._to == 'genes/ENSG00000188290'
+        LIMIT 5
+        FOR v IN variants
+            FILTER v._id == vg._from
+            RETURN v
     """
