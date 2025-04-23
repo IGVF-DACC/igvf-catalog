ac0321ba172c94a6636eb59a7ee932a39f91436f
Mingjie Li <mingjiecn@gmail.com>
2025-04-23 14:35:29 -0500

    update test

--- data/tests/test_AFGR_eqtl_adapter.py
+++ data/tests/test_AFGR_eqtl_adapter.py
@@ -1,4 +1,5 @@
 import json
+from unittest.mock import patch
 
 from adapters.AFGR_eqtl_adapter import AFGREQtl
 from adapters.writer import SpyWriter
@@ -6,13 +7,21 @@ from adapters.writer import SpyWriter
 
 def test_AFGR_eqtl_adapter_AFGR_eqtl():
     writer = SpyWriter()
-    adapter = AFGREQtl(filepath='./samples/AFGR/sorted.dist.hwe.af.AFR_META.eQTL.example.txt.gz',
-                       label='AFGR_eqtl', writer=writer)
-    adapter.process_file()
-    first_item = json.loads(writer.contents[0])
-    assert len(writer.contents) == 200
-    assert len(first_item) == 14
-    assert first_item['inverse_name'] == 'expression modulated by'
+    with patch('adapters.AFGR_eqtl_adapter.GeneValidator') as MockGeneValidator:
+        mock_validator_instance = MockGeneValidator.return_value
+        mock_validator_instance.validate.return_value = True
+
+        adapter = AFGREQtl(
+            filepath='./samples/AFGR/sorted.dist.hwe.af.AFR_META.eQTL.example.txt.gz',
+            label='AFGR_eqtl',
+            writer=writer
+        )
+        adapter.process_file()
+
+        first_item = json.loads(writer.contents[0])
+        assert len(writer.contents) == 200
+        assert len(first_item) == 14
+        assert first_item['inverse_name'] == 'expression modulated by'
 
 
 def test_AFGR_eqtl_adapter_AFGR_eqtl_term():
