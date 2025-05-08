5732ff27a4c76de03a03982dc70ad2d776881fbb
Mingjie Li <44071821+mingjiecn@users.noreply.github.com>
2025-05-08 11:56:42 -0500

    DSERV-833-mouse-mapping (#425)

--- data/data_loading_support_files/Biogrid_gene_gene/map_protein_to_gene.py
+++ data/data_loading_support_files/Biogrid_gene_gene/map_protein_to_gene.py
@@ -7,30 +7,38 @@ import csv
 import pickle
 
 
-def query_gene_from_protein(uniprot_id):
+def query_gene_from_protein(uniprot_id, species='Homo sapiens'):
     data_service_url = 'https://api-dev.catalog.igvf.org/api'
     endpoint = 'proteins/genes'
-    query_string = 'protein_id=' + uniprot_id
+    query_string = f'dbxrefs={uniprot_id}&organism={species}&verbose=true'
     url = data_service_url + '/' + endpoint + '?' + query_string
-
-    responses = requests.get(url).json()
-    genes = []
+    data = requests.get(url).json()
+    gene_ids = []
+    # deduplicate genes since one uniprot id can match more than one protein and each protein will target the same gene
+    if len(data) >= 0:
+        for gene_data in data:
+            gene_id = gene_data['gene']['_id']
+            if gene_id not in gene_ids:
+                gene_ids.append(gene_id)
     # for those with multiple mappings, only keep those on chr1-22, chrX, chrY
     # (i.e. filtered out scaffold genes)
-    if len(responses) > 1:
-        for response in responses:
-            if response['chr'].startswith('chr'):
-                genes.append(response['_id'])
-    elif len(responses) == 1:
-        genes.append(responses[0]['_id'])
+    if len(gene_ids) > 1:
+        updated_gene_ids = []
+        for gene_id in gene_ids:
+            for gene_data in data:
+                if gene_id == gene_data['gene']['_id']:
+                    if gene_data['gene']['chr'].startswith('chr'):
+                        updated_gene_ids.append(gene_id)
+                    break
+        gene_ids = updated_gene_ids
 
-    if len(genes) > 1:
+    if len(gene_ids) > 1:
         print(uniprot_id + ' has more than one match')
-    elif not genes:
+    elif not gene_ids:
         print(uniprot_id + ' has no match')
         return
 
-    return genes
+    return gene_ids
 
 
 # Human mappings
@@ -43,7 +51,8 @@ with open('merged_PPI.UniProt.csv', 'r') as interaction_file:
             proteins = row[:2]
             for protein in proteins:
                 if protein not in protein_to_gene.keys():
-                    gene = query_gene_from_protein(protein)
+                    gene = query_gene_from_protein(
+                        protein, species='Homo sapiens')
                     protein_to_gene[protein] = gene
 
 # manually fix unmapped proteins
@@ -64,8 +73,9 @@ with open('merged_PPI_mouse.UniProt.csv', 'r') as interaction_file:
         if row[3] == 'genetic interference':
             proteins = row[:2]
             for protein in proteins:
-                if protein not in protein_to_gene.keys():
-                    gene = query_gene_from_protein(protein)
+                if protein not in protein_to_gene_mouse.keys():
+                    gene = query_gene_from_protein(
+                        protein, species='Mus musculus')
                     protein_to_gene_mouse[protein] = gene
 output = open('biogrid_protein_mapping_mouse.pkl', 'wb')
 pickle.dump(protein_to_gene_mouse, output)
