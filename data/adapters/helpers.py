43233cc8ccc6de2c7dc6da4ff68e4fc068ffa5b3
Ian Whaling <78115078+ian-whaling@users.noreply.github.com>
2025-06-27 11:40:07 -0700

    DSERV-872-gersbach-perturb-seq (#444)
    * adapter + tests
    
    * reduced redundancy in code
    
    * skip if in dict
    
    * tested elements instead of promoters

--- data/adapters/helpers.py
+++ data/adapters/helpers.py
@@ -420,11 +420,11 @@ def get_ref_seq_by_spdi(spdi, species='human'):
     return seq_repo[chr_ref][start:end]
 
 
-def check_collection_loaded(collection, record_id):
+def check_collection_loaded(collection, record_id, timeout_seconds=1.0):
     try:
         db = ArangoDB().get_igvf_connection()
         col = db.collection(collection)
-        return col.has(record_id)
+        return col.has(record_id, timeout=timeout_seconds)
     except Exception as e:
         print(f'Error checking {record_id} in {collection}: {e}')
         return False
