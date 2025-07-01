d697a6eb86c37aa8de50ed1f3348287ead1cfa45
Ian Whaling <ian.whaling@gmail.com>
2025-06-30 18:23:28 -0700

    adapter + tests

--- data/active_adapters.py
+++ data/active_adapters.py
@@ -46,7 +46,8 @@ from adapters.VAMP_coding_variant_scores_adapter import VAMPAdapter
 from adapters.SEM_motif_adapter import SEMMotif
 from adapters.SEM_prediction_adapter import SEMPred
 from adapters.BlueSTARR_variant_elements_adapter import BlueSTARRVariantElement
-from adapters.Variant_EFFECTS_variant_gene_adapter import VariantEFFECTSAdapter
+from data.adapters.Variant_EFFECTS_variant_gene_adapter import VariantEFFECTSAdapter
+from adapters.STARR_seq_adapter import STARRseqVariantOntologyTerm
 from adapters.file_fileset_adapter import FileFileSet
 from adapters.eqtl_catalog_adapter import EQTLCatalog
 from adapters.SGE_variant_phenotype_adapter import SGE
@@ -137,6 +138,7 @@ LABEL_TO_ADAPTER = {
     'variant_disease_gene': ClinGen,
     'bluestarr_variant_elements': BlueSTARRVariantElement,
     'variant_effects_variant_gene': VariantEFFECTSAdapter,
+    'starr_seq_variant_ontology_term': STARRseqVariantOntologyTerm,
     'vamp_coding_variant_phenotype': VAMPAdapter,
     'ontology': Ontology,
     'SEM_motif': SEMMotif,
