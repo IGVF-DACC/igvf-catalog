8eaa2bad13299103381ae7a38a33e2ba9f5b9abc
Pedro Assis <pedroh2306@gmail.com>
2025-07-22 18:23:00 -1000

    using correct query

--- src/routers/datatypeRouters/edges/genes_coding_variants.ts
+++ src/routers/datatypeRouters/edges/genes_coding_variants.ts
@@ -2,89 +2,76 @@ import { z } from 'zod'
 import { db } from '../../../database'
 import { descriptions } from '../descriptions'
 import { loadSchemaConfig } from '../../genericRouters/genericRouters'
-import { genesCommonQueryFormat, commonHumanEdgeParamsFormat } from '../params'
-import { geneSearch } from '../nodes/genes'
 import { TRPCError } from '@trpc/server'
 import { paramsFormatType } from '../_helpers'
 import { publicProcedure } from '../../../trpc'
 
-const MAX_PAGE_SIZE = 100
+const QUERY_LIMIT = 500
+
+const geneQueryFormat = z.object({
+  gene_id: z.string().optional(),
+  page: z.number().default(0),
+  limit: z.number().optional()
+})
 
 const codingVariantsScoresFormat = z.object({
   coding_variant_id: z.string(),
-  scores: z.object({
-    SIFT_score: z.number().nullable(),
-    SIFT4G_score: z.number().nullable(),
-    Polyphen2_HDIV_score: z.number().nullable(),
-    Polyphen2_HVAR_score: z.number().nullable(),
-    VEST4_score: z.number().nullable(),
-    REVEL_score: z.number().nullable(),
-    MutPred_score: z.number().nullable(),
-    BayesDel_addAF_score: z.number().nullable(),
-    BayesDel_noAF_score: z.number().nullable(),
-    VARITY_R_score: z.number().nullable(),
-    VARITY_ER_score: z.number().nullable(),
-    VARITY_R_LOO_score: z.number().nullable(),
-    VARITY_ER_LOO_score: z.number().nullable(),
-    ESM1b_score: z.number().nullable(),
-    AlphaMissense_score: z.number().nullable(),
-    CADD_raw_score: z.number().nullable()
-  })
+  score: z.number().optional(),
+  source: z.string()
 })
 
 const schema = loadSchemaConfig()
 const codingVariantSchema = schema['coding variant']
+const codingVariantToPhenotypeSchema = schema['coding variant to phenotype']
+const geneSchema = schema.gene
 
 async function findCodingVariantsFromGenes (input: paramsFormatType): Promise<any[]> {
-  let limit = MAX_PAGE_SIZE
+  let limit = QUERY_LIMIT
   if (input.limit !== undefined) {
-    limit = (input.limit as number <= MAX_PAGE_SIZE) ? input.limit as number : MAX_PAGE_SIZE
+    limit = (input.limit as number <= QUERY_LIMIT) ? input.limit as number : QUERY_LIMIT
     delete input.limit
   }
 
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  const { gene_id, hgnc, gene_name: name, alias, organism } = input
-  const geneInput: paramsFormatType = { gene_id, hgnc, name, alias, organism, page: 0 }
-  delete input.hgnc
-  delete input.gene_name
-  delete input.alias
-  delete input.organism
-  const genes = await geneSearch(geneInput)
-  const geneNames = genes.map(gene => `${gene.name as string}`)
-
-  if (geneNames.length === 0) {
+  if (input.gene_id === undefined) {
     throw new TRPCError({
-      code: 'NOT_FOUND',
-      message: 'Gene not found.'
+      code: 'BAD_REQUEST',
+      message: 'gene_id is required'
     })
   }
 
   const query = `
-    FOR record IN ${codingVariantSchema.db_collection_name as string}
-    FILTER record.gene_name IN ['${geneNames.join(',')}']
-    SORT record._key
-    LIMIT ${input.page as number * limit}, ${limit}
-    RETURN {
-      coding_variant_id: record._key,
-      scores: {
-        SIFT_score: record.SIFT_score,
-        SIFT4G_score: record.SIFT4G_score,
-        Polyphen2_HDIV_score: record.Polyphen2_HDIV_score,
-        Polyphen2_HVAR_score: record.Polyphen2_HVAR_score,
-        VEST4_score: record.VEST4_score,
-        REVEL_score: record.REVEL_score,
-        MutPred_score: record.MutPred_score,
-        BayesDel_addAF_score: record.BayesDel_addAF_score,
-        BayesDel_noAF_score: record.BayesDel_noAF_score,
-        VARITY_R_score: record.VARITY_R_score,
-        VARITY_ER_score: record.VARITY_ER_score,
-        VARITY_R_LOO_score: record.VARITY_R_LOO_score,
-        VARITY_ER_LOO_score: record.VARITY_ER_LOO_score,
-        ESM1b_score: record.ESM1b_score,
-        AlphaMissense_score: record.AlphaMissense_score,
-        CADD_raw_score: record.CADD_raw_score
-      }
-    }
+    LET gene_name = DOCUMENT("${geneSchema.db_collection_name as string}/${input.gene_id as string}").name
+
+    LET codingVariants = (
+      FOR record IN ${codingVariantSchema.db_collection_name as string}
+      FILTER record.gene_name == gene_name
+      RETURN record._id
+    )
+
+    FOR variant IN UNION(
+      // SGE
+      FOR v IN variants_phenotypes_coding_variants
+        FILTER v._to IN codingVariants
+        LET files_filesets = DOCUMENT(v.files_filesets)
+        LET vPhenotype = DOCUMENT(v._from)
+        RETURN {
+          coding_variant_id: PARSE_IDENTIFIER(v._to).key,
+          score: vPhenotype.score,
+          source: files_filesets.preferred_assay_titles[0]
+        },
+
+      // VampSeq
+      FOR p IN ${codingVariantToPhenotypeSchema.db_collection_name as string}
+        FILTER p._from IN codingVariants
+        RETURN {
+          coding_variant_id: PARSE_IDENTIFIER(p._from).key,
+          score: p.abundance_score,
+          source: p.source
+        }
+    )
+      SORT variant.score DESC
+      LIMIT ${input.page as number * limit}, ${limit}
+      RETURN variant
   `
 
   return await ((await db.query(query)).all())
@@ -92,7 +79,7 @@ async function findCodingVariantsFromGenes (input: paramsFormatType): Promise<an
 
 const codingVariantsFromGenes = publicProcedure
   .meta({ openapi: { method: 'GET', path: '/genes/coding-variants', description: descriptions.genes_coding_variants } })
-  .input(genesCommonQueryFormat.merge(commonHumanEdgeParamsFormat).omit({ verbose: true, organism: true }))
+  .input(geneQueryFormat)
   .output(z.array(codingVariantsScoresFormat))
   .query(async ({ input }) => await findCodingVariantsFromGenes(input))
 
