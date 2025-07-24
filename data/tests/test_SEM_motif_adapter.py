0adab0cb84173d19c9465ef4601b179710abc114
Shengcheng Dong <shengchd@stanford.edu>
2025-07-24 10:28:48 -0700

    DSERV-874-load-new-SEMpl (#457)
    * DSERV-874-load-new-SEMpl
    
    * add source file
    
    * more in source yaml file
    
    * add complex loading part
    
    * remove old complex part
    
    * bug in file accession
    
    * bug fix in motif adapter
    
    * bug fix for prediction adapter
    
    * add missing prop to prediction
    
    * sample data and tests
    
    * drop unused function
    
    * fix typo files_filesets
    
    * drop files_filesets from model files
    
    * add file accessions to fileset yaml

--- data/tests/test_SEM_motif_adapter.py
+++ data/tests/test_SEM_motif_adapter.py
@@ -6,7 +6,8 @@ from adapters.writer import SpyWriter
 
 def test_sem_motif_adapter_motif():
     writer = SpyWriter()
-    adapter = SEMMotif(filepath='./samples/SEM/', label='motif', writer=writer)
+    adapter = SEMMotif(filepath='./samples/SEM/SEM_model_file.tsv.gz',
+                       sem_provenance_path='./samples/SEM/provenance_file.tsv.gz', label='motif', writer=writer)
     adapter.process_file()
     first_item = json.loads(writer.contents[0])
     assert len(writer.contents) > 0
@@ -17,14 +18,12 @@ def test_sem_motif_adapter_motif():
     assert 'source_url' in first_item
     assert 'pwm' in first_item
     assert 'length' in first_item
-    assert first_item['source'] == SEMMotif.SOURCE
-    assert first_item['source_url'] == SEMMotif.SOURCE_URL
 
 
 def test_sem_motif_adapter_motif_protein_link():
     writer = SpyWriter()
-    adapter = SEMMotif(filepath='./samples/SEM/',
-                       label='motif_protein_link', writer=writer)
+    adapter = SEMMotif(filepath='./samples/SEM/SEM_model_file.tsv.gz', sem_provenance_path='./samples/SEM/provenance_file.tsv.gz',
+                       label='motif_protein', writer=writer)
     adapter.process_file()
     first_item = json.loads(writer.contents[0])
     assert len(writer.contents) > 0
@@ -36,8 +35,6 @@ def test_sem_motif_adapter_motif_protein_link():
     assert 'biological_process' in first_item
     assert 'source' in first_item
     assert 'source_url' in first_item
-    assert first_item['source'] == SEMMotif.SOURCE
-    assert first_item['source_url'] == SEMMotif.SOURCE_URL
     assert first_item['name'] == 'is used by'
     assert first_item['inverse_name'] == 'uses'
     assert first_item['biological_process'] == 'ontology_terms/GO_0003677'
@@ -45,31 +42,14 @@ def test_sem_motif_adapter_motif_protein_link():
 
 def test_sem_motif_adapter_invalid_label():
     writer = SpyWriter()
-    with pytest.raises(ValueError, match='Invalid label. Allowed values: motif,motif_protein_link'):
-        SEMMotif(filepath='./samples/SEM/',
+    with pytest.raises(ValueError, match='Invalid label. Allowed values: motif,motif_protein,complex,complex_protein'):
+        SEMMotif(filepath='./samples/SEM/SEM_model_file.tsv.gz',
                  label='invalid_label', writer=writer)
 
 
-def test_sem_motif_adapter_initialization():
-    writer = SpyWriter()
-    for label in SEMMotif.ALLOWED_LABELS:
-        adapter = SEMMotif(filepath='./samples/SEM/',
-                           label=label, writer=writer)
-        assert adapter.filepath == './samples/SEM/'
-        assert adapter.label == label
-        assert adapter.dataset == label
-        assert adapter.dry_run == True
-        assert adapter.writer == writer
-        if label == 'motif':
-            assert adapter.type == 'node'
-            assert adapter.collection == 'motifs'
-        else:
-            assert adapter.type == 'edge'
-            assert adapter.collection == 'motifs_proteins'
-
-
 def test_sem_motif_adapter_load_tf_id_mapping():
-    adapter = SEMMotif(filepath='./samples/SEM/')
+    adapter = SEMMotif(filepath='./samples/SEM/SEM_model_file.tsv.gz',
+                       sem_provenance_path='./samples/SEM/provenance_file.tsv.gz')
     adapter.load_tf_id_mapping()
     assert hasattr(adapter, 'tf_id_mapping')
     assert isinstance(adapter.tf_id_mapping, dict)
