fd6e7366c6e6d969c80afa6f388e38d7d6100ad7
Mingjie Li <mingjiecn@gmail.com>
2025-07-30 11:19:13 -0500

    lazy loading gene ids for gene validator

--- data/adapters/gene_validator.py
+++ data/adapters/gene_validator.py
@@ -3,18 +3,23 @@ from db.arango_db import ArangoDB
 
 class GeneValidator:
     def __init__(self):
-        self.db = ArangoDB().get_igvf_connection()
-        self.valid_gene_ids = self._load_valid_gene_ids()
+        self._valid_gene_ids = None  # Lazy loading - will be loaded on first use
         self.invalid_gene_ids = set()
 
     def _load_valid_gene_ids(self):
-        cursor = self.db.aql.execute(
-            'FOR gene IN genes RETURN gene._key'
-        )
-        return set(cursor)
+        """Load valid gene IDs from database only when needed."""
+        if self._valid_gene_ids is None:
+            db = ArangoDB().get_igvf_connection()
+            cursor = db.aql.execute(
+                'FOR gene IN genes RETURN gene._key'
+            )
+            self._valid_gene_ids = set(cursor)
+        return self._valid_gene_ids
 
     def validate(self, gene_id: str) -> bool:
-        if gene_id not in self.valid_gene_ids:
+        """Validate a gene ID against the database."""
+        valid_ids = self._load_valid_gene_ids()
+        if gene_id not in valid_ids:
             self.invalid_gene_ids.add(gene_id)
             return False
         return True
