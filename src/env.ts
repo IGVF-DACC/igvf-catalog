44467a9dcc71b9f12d5174c5b4f96f26dadc3edc
Mingjie Li <mingjiecn@gmail.com>
2025-08-01 10:55:58 -0500

    Only load environment variables in production mode

--- src/env.ts
+++ src/env.ts
@@ -28,13 +28,17 @@ const environment = process.env.ENV ?? process.env.NODE_ENV
 
 if (typeof environment !== 'undefined') {
   config.environment = environment
-  config.host.protocol = process.env.IGVF_CATALOG_PROTOCOL ?? config.host.protocol
-  config.host.hostname = process.env.IGVF_CATALOG_HOSTNAME ?? config.host.hostname
-  config.host.port = parseInt(process.env.IGVF_CATALOG_PORT ?? DEFAULT_PORT) ?? config.host.port
-  config.database.connectionUri = process.env.IGVF_CATALOG_ARANGODB_URI ?? config.database.connectionUri
-  config.database.dbName = process.env.IGVF_CATALOG_ARANGODB_DBNAME ?? config.database.dbName
-  config.database.auth.username = process.env.IGVF_CATALOG_ARANGODB_USERNAME ?? config.database.auth.username
-  config.database.auth.password = process.env.IGVF_CATALOG_ARANGODB_PASSWORD ?? config.database.auth.password
+
+  // Only load environment variables in production mode
+  if (environment === 'production') {
+    config.host.protocol = process.env.IGVF_CATALOG_PROTOCOL ?? config.host.protocol
+    config.host.hostname = process.env.IGVF_CATALOG_HOSTNAME ?? config.host.hostname
+    config.host.port = parseInt(process.env.IGVF_CATALOG_PORT ?? DEFAULT_PORT) ?? config.host.port
+    config.database.connectionUri = process.env.IGVF_CATALOG_ARANGODB_URI ?? config.database.connectionUri
+    config.database.dbName = process.env.IGVF_CATALOG_ARANGODB_DBNAME ?? config.database.dbName
+    config.database.auth.username = process.env.IGVF_CATALOG_ARANGODB_USERNAME ?? config.database.auth.username
+    config.database.auth.password = process.env.IGVF_CATALOG_ARANGODB_PASSWORD ?? config.database.auth.password
+  }
 }
 
 const env = envSchema.safeParse(config)
