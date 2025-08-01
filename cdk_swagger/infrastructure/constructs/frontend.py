63a80f2c6102223903342b2f313a9665c32dabda
Mingjie Li <mingjiecn@gmail.com>
2025-07-31 20:04:45 -0500

    update _define_docker_assets

--- cdk_swagger/infrastructure/constructs/frontend.py
+++ cdk_swagger/infrastructure/constructs/frontend.py
@@ -69,8 +69,8 @@ class Frontend(Construct):
 
     def _define_docker_assets(self) -> None:
         self.application_image = ContainerImage.from_asset(
-            '../docker/api/',
-            file='Dockerfile.prod',
+            '../',
+            file='docker/api/Dockerfile.prod',
         )
         self.nginx_image = ContainerImage.from_asset(
             '../docker/nginx/'
