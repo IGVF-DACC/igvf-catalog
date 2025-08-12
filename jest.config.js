e4236f6b0aebb209958f1aa66285748defb55a6f
Mingjie Li <mingjiecn@gmail.com>
2025-08-12 12:38:12 -0500

    set up coveralls

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,27 @@ module.exports = {
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   collectCoverage: true,
   clearMocks: true,
-  coverageDirectory: 'coverage'
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/env.ts',
+    'src/trpc.ts',
+    'src/routers/datatypeRouters/**/*.ts',
+    '!src/routers/datatypeRouters/**/*.d.ts',
+    '!src/routers/datatypeRouters/**/__tests__/**',
+    '!src/routers/datatypeRouters/**/*.test.ts',
+    '!src/routers/datatypeRouters/**/*.spec.ts'
+  ],
+  coverageReporters: ['lcov', 'text'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    '/coverage/',
+    '/cdk_swagger/cdk.out/',
+    '\\.d\\.ts$',
+    '\\.d\\.js$'
+  ],
+  modulePathIgnorePatterns: [
+    '/dist/',
+    '/cdk_swagger/cdk.out/'
+  ]
 }
