26a4b6ed56a1b4f3d5502e4acf0ece8a0c0fd816
Mingjie Li <mingjiecn@gmail.com>
2025-12-02 17:38:11 -0600

    update genomic_elements_genes parsed by ENCODE2GCRISPR

--- data/tests/test_encode_E2G_CRISPR_adapter.py
+++ data/tests/test_encode_E2G_CRISPR_adapter.py
@@ -5,8 +5,22 @@ from adapters.encode_E2G_CRISPR_adapter import ENCODE2GCRISPR
 from adapters.writer import SpyWriter
 
 
+# mock get_file_fileset_by_accession_in_arangodb so files_fileset data change will not affect the test
+@pytest.fixture
+def mock_file_fileset():
+    """Fixture to mock get_file_fileset_by_accession_in_arangodb function."""
+    with patch('adapters.encode_E2G_CRISPR_adapter.get_file_fileset_by_accession_in_arangodb') as mock_get_file_fileset:
+        mock_get_file_fileset.return_value = {
+            'method': 'CRISPR enhancer perturbation screens',
+            'class': 'observed data',
+            'simple_sample_summaries': ['K562'],
+            'samples': ['ontology_terms/EFO_0002067']
+        }
+        yield mock_get_file_fileset
+
+
 @pytest.mark.external_dependency
-def test_encode2gcrispr_adapter_regulatory_region():
+def test_encode2gcrispr_adapter_regulatory_region(mock_file_fileset):
     writer = SpyWriter()
     adapter = ENCODE2GCRISPR(
         filepath='./samples/ENCODE_E2G_CRISPR_example.tsv', label='genomic_element', writer=writer, validate=True)
@@ -23,7 +37,7 @@ def test_encode2gcrispr_adapter_regulatory_region():
 
 
 @pytest.mark.external_dependency
-def test_encode2gcrispr_adapter_regulatory_region_gene():
+def test_encode2gcrispr_adapter_regulatory_region_gene(mock_file_fileset):
     writer = SpyWriter()
     adapter = ENCODE2GCRISPR(filepath='./samples/ENCODE_E2G_CRISPR_example.tsv',
                              label='genomic_element_gene', writer=writer, validate=True)
@@ -39,17 +53,21 @@ def test_encode2gcrispr_adapter_regulatory_region_gene():
     assert 'significant' in first_item
     assert first_item['source'] == ENCODE2GCRISPR.SOURCE
     assert first_item['source_url'] == ENCODE2GCRISPR.SOURCE_URL
-    assert first_item['biological_context'] == f'ontology_terms/{ENCODE2GCRISPR.BIOLOGICAL_CONTEXT}'
+    assert first_item['biological_context'] == 'K562'
+    assert first_item['biosample_term'] == 'ontology_terms/EFO_0002067'
+    assert first_item['label'] == 'regulatory element effect on gene expression'
+    assert first_item['method'] == 'CRISPR enhancer perturbation screens'
+    assert first_item['class'] == 'observed data'
 
 
-def test_encode2gcrispr_adapter_invalid_label():
+def test_encode2gcrispr_adapter_invalid_label(mock_file_fileset):
     writer = SpyWriter()
     with pytest.raises(ValueError, match='Invalid label: invalid_label. Allowed values: genomic_element, genomic_element_gene'):
         ENCODE2GCRISPR(filepath='./samples/ENCODE_E2G_CRISPR_example.tsv',
                        label='invalid_label', writer=writer)
 
 
-def test_encode2gcrispr_adapter_initialization():
+def test_encode2gcrispr_adapter_initialization(mock_file_fileset):
     writer = SpyWriter()
     for label in ENCODE2GCRISPR.ALLOWED_LABELS:
         adapter = ENCODE2GCRISPR(
@@ -60,7 +78,7 @@ def test_encode2gcrispr_adapter_initialization():
         assert adapter.files_filesets is not None
 
 
-def test_encode2gcrispr_adapter_load_regulatory_region():
+def test_encode2gcrispr_adapter_load_regulatory_region(mock_file_fileset):
     writer = SpyWriter()
     adapter = ENCODE2GCRISPR(
         filepath='./samples/ENCODE_E2G_CRISPR_example.tsv', label='genomic_element', writer=writer)
@@ -70,7 +88,7 @@ def test_encode2gcrispr_adapter_load_regulatory_region():
     assert len(adapter.genomic_element_nodes) > 0
 
 
-def test_encode2gcrispr_adapter_load_gene_id_mapping():
+def test_encode2gcrispr_adapter_load_gene_id_mapping(mock_file_fileset):
     writer = SpyWriter()
     adapter = ENCODE2GCRISPR(filepath='./samples/ENCODE_E2G_CRISPR_example.tsv',
                              label='genomic_element_gene', writer=writer)
@@ -80,7 +98,7 @@ def test_encode2gcrispr_adapter_load_gene_id_mapping():
     assert len(adapter.gene_id_mapping) > 0
 
 
-def test_encode2gcrispr_adapter_validate_doc_invalid():
+def test_encode2gcrispr_adapter_validate_doc_invalid(mock_file_fileset):
     writer = SpyWriter()
     adapter = ENCODE2GCRISPR(filepath='./samples/ENCODE_E2G_CRISPR_example.tsv',
                              label='genomic_element_gene', writer=writer, validate=True)
