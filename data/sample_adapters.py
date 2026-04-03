5e54cbd18d9a5a454bf0658aa96eb2e819e87e91
Mingjie Li <mingjiecn@gmail.com>
2026-04-03 09:47:04 -0500

    remove BlueSTARR_variant_elements_adapter and related schemas

--- data/sample_adapters.py
+++ data/sample_adapters.py
@@ -36,7 +36,6 @@ from adapters.gencode_gene_structure_adapter import GencodeStructure
 from adapters.VAMP_coding_variant_scores_adapter import VAMPAdapter
 from adapters.SEM_motif_adapter import SEMMotif
 from adapters.SEM_prediction_adapter import SEMPred
-from adapters.BlueSTARR_variant_elements_adapter import BlueSTARRVariantElement
 from adapters.file_fileset_adapter import FileFileSet
 
 ADAPTERS = {
@@ -106,7 +105,6 @@ ADAPTERS = {
     'mouse_variant': MouseGenomesProjectAdapter(filepath='./samples/mouse_variants/mouse_variant_snps_rsid_sample.vcf'),
     'variant_disease': ClinGen('./samples/clinGen_variant_pathogenicity_example.csv', label='variant_disease'),
     'variant_disease_gene': ClinGen('./samples/clinGen_variant_pathogenicity_example.csv', label='variant_disease_gene'),
-    'bluestarr_variant_elements': BlueSTARRVariantElement('./samples/variant_phenotype_V2F.example.tsv', label='variant_genomic_element'),
     'vamp_coding_variant_phenotype': VAMPAdapter('./samples/vamp_coding_variants.example.csv', label='vamp_coding_variants_phenotypes'),
     'SEM_motif': SEMMotif('./samples/SEM/', label='motif'),
     'SEM_motif_protein': SEMMotif('./samples/SEM/', label='motif_protein_link'),
