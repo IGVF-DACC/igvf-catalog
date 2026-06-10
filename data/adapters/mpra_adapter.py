dd716311e5c93645b9a45d2ff3f745a27ede300b
Pedro Assis <pedroh2306@gmail.com>
2026-06-10 11:26:18 -1000

    qvalue -> pvalue_adj

--- data/adapters/mpra_adapter.py
+++ data/adapters/mpra_adapter.py
@@ -540,7 +540,7 @@ class MPRAAdapter(BaseAdapter):
                         'DNA_count': self.safe_float(row[7]),
                         'RNA_count': self.safe_float(row[8]),
                         'neg_log10_pvalue': minus_p,
-                        'neg_log10_qvalue': minus_q_edge,
+                        'neg_log10_pvalue_adj': minus_q_edge,
                         'significant': significant,
                         'class': self.collection_class,
                         'label': self.collection_label_elements_biosamples,
