ea060ff7e6eddc80324bc1e4416beb3b27d00fad
Otto Jolanki <ojolanki@stanford.edu>
2026-07-02 11:34:29 -0700

    file_accession

--- data/adapters/Mutpred2_coding_variants_adapter.py
+++ data/adapters/Mutpred2_coding_variants_adapter.py
@@ -64,7 +64,7 @@ class Mutpred2CodingVariantsScores(BaseAdapter):
             return 'coding_variants_phenotypes'
 
     def parse(self):
-        self.writer.add_tag('portal_accessions', self.FILE_ACCESSION)
+        self.writer.add_tag('portal_accessions', self.file_accession)
 
         # write all enumerated variants to jsonl files for variants, and variants_coding_variants collections
         # skip checking if they are already loaded since there are > 1,000 million records to check here, will deduplicate when loading them into database
