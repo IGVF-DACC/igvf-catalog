1d27c8e8ba4116f66fb756049a5873e50e230683
Otto Jolanki <ojolanki@stanford.edu>
2026-07-29 16:39:52 -0700

    make script non-specific about the key of the tag

--- data/scripts/tag_s3_data.py
+++ data/scripts/tag_s3_data.py
@@ -1,4 +1,4 @@
-# This script appends a value to the 'version' tag of S3 objects in a specified bucket.
+# This script appends a value to a specified tag (key_name) of S3 objects in a specified bucket.
 # It checks if the tag already exists and updates it accordingly.
 # If the tag does not exist, it creates a new one. The script can run in dry-run mode to preview changes without applying them.
 # Useful when deploying new versions of the IGVF Catalog in S3.
@@ -23,7 +23,7 @@ APPEND_VALUE = 'IGVF_catalog_v1.0.0'
 KEY_NAME = 'version'
 
 
-def append_to_version_tag(bucket_name, key_name, append_value, dry_run=True, profile=None):
+def append_to_tag(bucket_name, key_name, append_value, dry_run=True, profile=None):
     session = boto3.Session(
         profile_name=profile) if profile else boto3.Session()
     s3 = session.client('s3')
@@ -47,7 +47,7 @@ def append_to_version_tag(bucket_name, key_name, append_value, dry_run=True, pro
                 continue
 
             updated_tags = []
-            version_found = False
+            tag_found = False
 
             for tag in current_tags:
                 if tag['Key'] == key_name:
@@ -58,14 +58,14 @@ def append_to_version_tag(bucket_name, key_name, append_value, dry_run=True, pro
                     else:
                         existing_values.append(append_value)
                         tag['Value'] = ' '.join(existing_values)
-                        print(f"Updated 'version' tag to: {tag['Value']}")
-                    version_found = True
+                        print(f"Updated {key_name} tag to: {tag['Value']}")
+                    tag_found = True
                 updated_tags.append(tag)
 
-            if not version_found:
-                new_version = append_value.lstrip(' ')
-                updated_tags.append({'Key': 'version', 'Value': new_version})
-                print(f"Added new 'version' tag with value: {new_version}")
+            if not tag_found:
+                new_value = append_value.lstrip(' ')
+                updated_tags.append({'Key': key_name, 'Value': new_value})
+                print(f'Added new {key_name} tag with value: {new_value}')
 
             if dry_run:
                 print(f'[DRY RUN] Would apply tags to {key}: {updated_tags}')
@@ -83,13 +83,13 @@ def append_to_version_tag(bucket_name, key_name, append_value, dry_run=True, pro
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='Append a value to the version tag of S3 objects.')
+        description='Append a value to the tag specified by key_name of S3 objects.')
     parser.add_argument(
         '--bucket_name', help='The name of the S3 bucket.', default=BUCKET_NAME)
     parser.add_argument(
-        '--append_value', help='The value to append to the version tag.', default=APPEND_VALUE)
+        '--append_value', help='The value to append to the tag specified by key_name.', default=APPEND_VALUE)
     parser.add_argument(
-        '--key_name', help='The key name for the version tag.', default=KEY_NAME)
+        '--key_name', help='The key name for the tag to append to.', default=KEY_NAME)
     parser.add_argument('--dry-run', action='store_true',
                         help='If set, will not apply changes.', default=False)
     parser.add_argument(
@@ -97,5 +97,5 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    append_to_version_tag(args.bucket_name, args.key_name,
-                          args.append_value, dry_run=args.dry_run, profile=args.profile)
+    append_to_tag(args.bucket_name, args.key_name,
+                  args.append_value, dry_run=args.dry_run, profile=args.profile)
