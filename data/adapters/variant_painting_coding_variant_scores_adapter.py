3875b7f4ae9ee9b982ebc04e94c76391e4016597
shengchd <shengchd@umich.edu>
2026-08-03 11:37:37 -0700

    update adapter parse

--- data/adapters/variant_painting_coding_variant_scores_adapter.py
+++ data/adapters/variant_painting_coding_variant_scores_adapter.py
@@ -83,8 +83,7 @@ class VariantPaintingAdapter(BaseAdapter):
                 for skipped in skipped_coding_variants:
                     skipped_list.write(skipped + '\n')
 
-    def process_file(self):
-        self.writer.open()
+    def parse(self):
         file_fileset_obj = get_file_fileset_by_accession_in_arangodb(
             self.file_accession)
         if file_fileset_obj is None:
@@ -108,5 +107,3 @@ class VariantPaintingAdapter(BaseAdapter):
                 if self.label == 'coding_variants_phenotypes':
                     self.process_coding_variant_phenotype_chunk(
                         chunk, file_fileset_obj)
-
-        self.writer.close()
