7fff2fbb8ea4268b508b8f5e7c034d4bd9387536
Mingjie Li <44071821+mingjiecn@users.noreply.github.com>
2026-08-31 08:49:14 -0500

    DSERV-1527-gaf (#833)

--- data/tests/test_gaf_adapter.py
+++ data/tests/test_gaf_adapter.py
@@ -15,17 +15,27 @@ def mock_file_fileset():
         yield mock_get
 
 
-def test_gaf_adapter_human(mock_file_fileset):
+@pytest.fixture
+def mock_protein_map():
+    with patch('adapters.gaf_adapter.get_protein_map_from_arangodb') as mock_get:
+        mock_get.return_value = {
+            'A0A024RBG1': ['ENSP00000263100'],
+            'MGI:1915609': ['ENSMUSP00000000001'],
+        }
+        yield mock_get
+
+
+def test_gaf_adapter_human(mock_file_fileset, mock_protein_map):
     writer = SpyWriter()
     adapter = GAF(filepath='./samples/goa_human_sample.gaf.gz',
                   label='human', writer=writer, validate=True)
     adapter.file_accession = 'IGVFFI1490WZCV'
     adapter.process_file()
     mock_file_fileset.assert_called_once_with('IGVFFI1490WZCV')
+    mock_protein_map.assert_called_once_with(organism='Homo sapiens')
     first_item = json.loads(writer.contents[0])
     assert len(writer.contents) > 0
-    assert '_key' in first_item
-    assert '_from' in first_item
+    assert first_item['_from'] == 'proteins/ENSP00000263100'
     assert '_to' in first_item
     assert first_item['organism'] == 'Homo sapiens'
     assert first_item['source'] == 'Gene Ontology'
@@ -36,17 +46,18 @@ def test_gaf_adapter_human(mock_file_fileset):
     assert first_item['files_filesets'] == 'files_filesets/IGVFFI1490WZCV'
 
 
-def test_gaf_adapter_mouse(mock_file_fileset):
+def test_gaf_adapter_mouse(mock_file_fileset, mock_protein_map):
     writer = SpyWriter()
     adapter = GAF(filepath='./samples/mgi_sample.gaf.gz',
                   label='mouse', writer=writer, validate=True)
     adapter.file_accession = 'IGVFFI9807JOKT'
     adapter.process_file()
     mock_file_fileset.assert_called_once_with('IGVFFI9807JOKT')
+    mock_protein_map.assert_called_once_with(
+        dbxref_name='MGI', organism='Mus musculus')
     first_item = json.loads(writer.contents[0])
     assert len(writer.contents) > 0
-    assert '_key' in first_item
-    assert '_from' in first_item
+    assert first_item['_from'] == 'proteins/ENSMUSP00000000001'
     assert '_to' in first_item
     assert first_item['organism'] == 'Mus musculus'
     assert first_item['source'] == 'Gene Ontology'
@@ -66,8 +77,7 @@ def test_gaf_adapter_rna(mock_file_fileset):
     mock_file_fileset.assert_called_once_with('IGVFFI6501YXMX')
     first_item = json.loads(writer.contents[0])
     assert len(writer.contents) > 0
-    assert '_key' in first_item
-    assert '_from' in first_item
+    assert first_item['_from'].startswith('transcripts/')
     assert '_to' in first_item
     assert first_item['organism'] == 'Homo sapiens'
     assert first_item['source'] == 'Gene Ontology'
@@ -93,16 +103,6 @@ def test_gaf_adapter_load_rnacentral_mapping():
     assert hasattr(adapter, 'rnacentral_mapping')
     assert isinstance(adapter.rnacentral_mapping, dict)
     assert len(adapter.rnacentral_mapping) > 0
-
-
-def test_gaf_adapter_load_mouse_mgi_to_uniprot():
-    writer = SpyWriter()
-    adapter = GAF(filepath='./samples/mgi_sample.gaf.gz',
-                  label='mouse', writer=writer, validate=True)
-    adapter.load_mouse_mgi_to_uniprot()
-    assert hasattr(adapter, 'mouse_mgi_mapping')
-    assert isinstance(adapter.mouse_mgi_mapping, dict)
-    assert len(adapter.mouse_mgi_mapping) > 0
 
 
 def test_gaf_adapter_validate_doc_invalid():
