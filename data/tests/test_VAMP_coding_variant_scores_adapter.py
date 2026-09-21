a2491491dae7d3ff9bc4b5f3955ff41efa0005d0
Ian Whaling <ian.whaling@gmail.com>
2026-09-21 16:48:42 -0700

    test fixed

--- data/tests/test_VAMP_coding_variant_scores_adapter.py
+++ data/tests/test_VAMP_coding_variant_scores_adapter.py
@@ -25,7 +25,7 @@ MOCKED_GET_FILES_FILESET_ARANGO_RETURN = {
     'class': 'observed data',
     'label': 'protein variant effect',
     'simple_sample_summaries': ['test_summaries'],
-    'samples': ['ontology_terms/test_sample']
+    'samples': ['test_sample']
 }
 
 
