658a43f6827b93f2488792060d69c1c197de4e3a
Mingjie Li <44071821+mingjiecn@users.noreply.github.com>
2026-09-23 14:04:09 -0500

    DSERV-1587-mix-api (#886)

--- src/routers/datatypeRouters/edges/variants_phenotypes.ts
+++ src/routers/datatypeRouters/edges/variants_phenotypes.ts
@@ -81,6 +81,19 @@ const igvfVariantPhenotypeFormat = z.object({
   p_value_adj: z.number().nullish(),
   neg_log10_pvalue_adj: z.number().nullish(),
   crispr_modality: z.string().nullish(),
+  // SGE-specific
+  protein_id: z.string().nullish(),
+  exon: z.string().nullish(),
+  target: z.string().nullish(),
+  consequence: z.string().nullish(),
+  standard_error: z.number().nullish(),
+  '95_ci_upper': z.number().nullish(),
+  '95_ci_lower': z.number().nullish(),
+  amino_acid_change: z.string().nullish(),
+  hgvs_p: z.string().nullish(),
+  functional_consequence: z.string().nullish(),
+  functional_consequence_zscore: z.number().nullish(),
+  variant_qc_flag: z.string().nullish(),
   method: z.string().nullable(),
   class: z.string().nullish(),
   label: z.string().nullish(),
@@ -101,6 +114,7 @@ const studyCollectionName = studySchema.db_collection_name as string
 const variantPhenotypeGwasSchema = getSchema('data/schemas/edges/variants_phenotypes.GWAS.json')
 const variantsPhenotypeNonGwasSchema = getSchema('data/schemas/edges/variants_phenotypes.cV2F.json')
 const variantsPhenotypeCrisprSchema = getSchema('data/schemas/edges/variants_phenotypes.CRISPRVariantPhenotype.json')
+const variantsPhenotypeSgeSchema = getSchema('data/schemas/edges/variants_phenotypes.SGE.json')
 
 function valueValidation (input: paramsFormatType): void {
   if (input.neg_log10_pvalue !== undefined) {
@@ -195,54 +209,25 @@ async function findVariantsFromPhenotypesSearch (input: paramsFormatType): Promi
     LIMIT ${input.page as number * limit}, ${limit}
 
     ${input.verbose === 'true' ? 'LET variant = DOCUMENT(record._from)' : ''}
-    LET phenotype_name = DOCUMENT(record._to).name
-    RETURN {
-      variant:      ${input.verbose === 'true' ? variantVerboseFields : 'record._from'},
-      phenotype_id: record._to,
-      name:         record.name,
-      source:       record.source,
-      source_url:   record.source_url,
-      class:        record.class,
-      method:       record.method,
-      label:        record.label,
-      phenotype_term: phenotype_name,
-
-      // OpenTargets-specific
-      version:        record.source == 'OpenTargets' ? record.version        : null,
-      lead_chrom:     record.source == 'OpenTargets' ? record.lead_chrom     : null,
-      lead_pos:       record.source == 'OpenTargets' ? record.lead_pos       : null,
-      lead_ref:       record.source == 'OpenTargets' ? record.lead_ref       : null,
-      lead_alt:       record.source == 'OpenTargets' ? record.lead_alt       : null,
-      direction:      record.source == 'OpenTargets' ? record.direction      : null,
-      beta:           record.source == 'OpenTargets' ? record.beta           : null,
-      beta_ci_lower:  record.source == 'OpenTargets' ? record.beta_ci_lower  : null,
-      beta_ci_upper:  record.source == 'OpenTargets' ? record.beta_ci_upper  : null,
-      p_val_mantissa: record.source == 'OpenTargets' ? record.p_val_mantissa : null,
-      p_val_exponent: record.source == 'OpenTargets' ? record.p_val_exponent : null,
-      p_value: record.source == 'OpenTargets' ? record.p_value : null,
-      neg_log10_pvalue: record.source == 'OpenTargets' ? record.neg_log10_pvalue : null,
-      oddsr_ci_lower: record.source == 'OpenTargets' ? record.oddsr_ci_lower : null,
-      oddsr_ci_upper: record.source == 'OpenTargets' ? record.oddsr_ci_upper : null,
-      study:          record.source == 'OpenTargets' ? ${input.verbose === 'true' ? `(${studyVerboseQuery})[0]` : 'record.study'} : null,
-
-      // non-OpenTargets specific
-      score:              record.source != 'OpenTargets' ? record.score              : null,
-      files_filesets:     record.source != 'OpenTargets' ? record.files_filesets     : null,
-      biosample_term:     record.source != 'OpenTargets' ? record.biosample_term     : null,
-      biological_context: record.source != 'OpenTargets' ? record.biological_context : null,
-
-      // CRISPR variant phenotype specific
-      effect_size:        record.method == 'CRISPR screen' ? record.effect_size        : null,
-      z_score:            record.method == 'CRISPR screen' ? record.z_score            : null,
-      significant:        record.method == 'CRISPR screen' ? record.significant        : null,
-      num_guides:         record.method == 'CRISPR screen' ? record.num_guides         : null,
-      edit_rate_mean:     record.method == 'CRISPR screen' ? record.edit_rate_mean     : null,
-      effect_size_ci95_lower: record.method == 'CRISPR screen' ? record.effect_size_ci95_lower : null,
-      effect_size_ci95_upper: record.method == 'CRISPR screen' ? record.effect_size_ci95_upper : null,
-      p_value_adj:        record.method == 'CRISPR screen' ? record.p_value_adj        : null,
-      neg_log10_pvalue_adj: record.method == 'CRISPR screen' ? record.neg_log10_pvalue_adj : null,
-      crispr_modality:    record.method == 'CRISPR screen' ? record.crispr_modality    : null
-    }
+    RETURN MERGE(
+      {
+        variant: ${input.verbose === 'true' ? variantVerboseFields : 'record._from'},
+        phenotype_id: record._to,
+      },
+      (record.source == 'OpenTargets' ? {
+        study: ${input.verbose === 'true' ? `(${studyVerboseQuery})[0]` : 'record.study'},
+        ${getDBReturnStatements(variantPhenotypeGwasSchema, false, '', ['study'])}
+      } : (record.method == 'CRISPR screen' ? {
+        ${getDBReturnStatements(variantsPhenotypeCrisprSchema)},
+        phenotype_term: DOCUMENT(record._to).name
+      } : (record.method == 'SGE' ? {
+        ${getDBReturnStatements(variantsPhenotypeSgeSchema)},
+        phenotype_term: DOCUMENT(record._to).name
+      } : {
+        ${getDBReturnStatements(variantsPhenotypeNonGwasSchema)},
+        phenotype_term: DOCUMENT(record._to).name
+      })))
+    )
   `
 
   let result = []
@@ -316,14 +301,17 @@ async function findPhenotypesFromVariantSearch (input: paramsFormatType): Promis
 
         (record.source == 'OpenTargets' ? {
           study: ${input.verbose === 'true' ? `(${studyVerboseQuery})[0]` : 'record.study'},
-          ${getDBReturnStatements(variantPhenotypeGwasSchema)}
+          ${getDBReturnStatements(variantPhenotypeGwasSchema, false, '', ['study'])}
         } : (record.method == 'CRISPR screen' ? {
           ${getDBReturnStatements(variantsPhenotypeCrisprSchema)},
+          phenotype_term: DOCUMENT(record._to).name
+        } : (record.method == 'SGE' ? {
+          ${getDBReturnStatements(variantsPhenotypeSgeSchema)},
           phenotype_term: DOCUMENT(record._to).name
         } : {
           ${getDBReturnStatements(variantsPhenotypeNonGwasSchema)},
           phenotype_term: DOCUMENT(record._to).name
-        }))
+        })))
     )
   `
 
