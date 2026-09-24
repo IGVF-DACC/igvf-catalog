8636618f778d77729bdd11252957c8046d4ff8f6
Pedro Assis <pedroh2306@gmail.com>
2026-09-24 10:57:21 -1000

    removing deprecated calls

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   roots: ['<rootDir>'],
   transform: {
-    '^.+\\.ts?$': ['ts-jest', { isolatedModules: true }]
+    '^.+\\.ts?$': 'ts-jest'
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
--- src/__tests__/env.test.ts
+++ src/__tests__/env.test.ts
@@ -1,3 +1,5 @@
+export {}
+
 describe('env.ts', () => {
   const OLD_ENV = process.env
 
